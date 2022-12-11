271c12c22bf7f37e82ac160fb4fda69199132089
Flizzyclone <stormry20@gmail.com>
2022-12-11 01:15:06 -0500

    changing how CandidateBox is disabled

--- client/src/lib/components/candidatebox/CandidateBoxContainer.svelte
+++ client/src/lib/components/candidatebox/CandidateBoxContainer.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import CandidateBox from './CandidateBox.svelte';
-	import NonEditableCandidateBox from './NonEditableCandidateBox.svelte';
 	import { TossupCandidateStore, CandidatesStore } from '$lib/stores/Candidates';
 </script>
 
 <div class="flex flex-row flex-wrap justify-center relative pointer-events-none h-0 z-10">
-	<NonEditableCandidateBox candidate={$TossupCandidateStore} />
+	<CandidateBox candidate={$TossupCandidateStore} editable={false}/>
 	{#each $CandidatesStore as candidate}
-		<CandidateBox {candidate} />
+		<CandidateBox {candidate} editable={true}/>
 	{/each}
 </div>
