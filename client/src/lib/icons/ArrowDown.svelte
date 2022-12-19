9dfcd35e64d5332d23136a397ad1b8955c67e4bf
Jacob Enders <jacob@Jacob-Desktop>
2022-12-18 21:56:40 -0500

    prettier fix

--- client/src/lib/icons/ArrowDown.svelte
+++ client/src/lib/icons/ArrowDown.svelte
@@ -9,7 +9,7 @@
 	stroke-width="1.5"
 	stroke="currentColor"
 	class="w-6 h-6"
-  style={`color: ${color}`}
+	style={`color: ${color}`}
 >
 	<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
 </svg>
