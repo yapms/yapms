3c762a3b04d759fc9a4aa3c1b7077afc078edf38
Jacob Enders <jacobenders1@gmail.com>
2022-12-22 23:11:07 -0500

    show colors of margins under candidate name

--- client/src/lib/components/candidatebox/CandidateBox.svelte
+++ client/src/lib/components/candidatebox/CandidateBox.svelte
@@ -10,7 +10,6 @@
 	export let editable: boolean;
 
 	$: selected = $SelectedCandidateStore.id === candidate.id;
-	$: buttonStyle = selected ? 'btn btn-sm' : 'btn btn-xs';
 
 	function updateSelectedCandidate() {
 		SelectedCandidateStore.set(candidate);
@@ -26,16 +25,33 @@
 
 <div class="btn-group p-0.5 pointer-events-auto">
 	<button
-		class={buttonStyle + ' no-animation'}
+		class="btn no-animation overflow-hidden p-0"
+		class:btn-sm={selected}
+		class:btn-xs={!selected}
 		style:background-color={candidate.margins[0].color}
 		style:color={calculateLumaHEX(candidate.margins[0].color) > 0.5 ? 'black' : 'white'}
 		style="transition: all 0.25s"
 		on:click={updateSelectedCandidate}
 	>
-		{candidate.name} - {$CandidateCounts.get(candidate.id) ?? 0}
+		<div class="flex flex-col gap-0">
+			<div class="pt-1 pb-1 pl-2 pr-2">
+				{candidate.name} {$CandidateCounts.get(candidate.id) ?? 0}
+			</div>
+			{#if candidate.margins.length > 1}
+				<div class="flex flex-row w-full">
+					{#each candidate.margins as margin}
+						<div class="w-full min-h-6"
+							style:background-color={margin.color}
+						/>
+					{/each}
+				</div>
+			{/if}
+		</div>
 	</button>
 	<button
-		class={buttonStyle + ' no-animation'}
+		class="btn no-animation"
+		class:btn-sm={selected}
+		class:btn-xs={!selected}
 		disabled={!editable}
 		style="transition: all 0.25s"
 		on:click={openEditCandidateModal}
