d82d55bf49305bef992075635be90cb22d4e7433
Jacob Enders <jacobenders1@gmail.com>
2022-12-24 13:28:25 -0500

    eslint shared package

--- apps/yapms/.eslintrc.cjs
+++ apps/yapms/.eslintrc.cjs
@@ -1,20 +1,8 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['@yapms/eslint-config', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs', 'tsconfig.json', 'svelte.config.js'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	extends: ['yapms'],
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname
 	}
 };
