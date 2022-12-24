f8466e9b2b005b003a29f0a21fe13d267807202f
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2022-12-24 14:47:04 -0500

    Fixes to map loading (#70)

--- apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/ApplyPanZoom.ts
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/ApplyPanZoom.ts
@@ -1,11 +1,11 @@
 import panzoom, { type PanZoom } from 'panzoom';
 
 function applyPanZoom(mapBind: HTMLDivElement): PanZoom | undefined {
-	const svg = mapBind.querySelector<SVGElement>('#testing-map');
+	const svg = mapBind.querySelector<SVGElement>('svg'); //We can do this since the only child of node is the map svg itself.
 	if (svg) {
 		return panzoom(svg, {
 			minZoom: 0.5,
-			maxZoom: 2,
+			maxZoom: 100,
 			smoothScroll: false,
 			autocenter: true,
 			zoomDoubleClickSpeed: 1
