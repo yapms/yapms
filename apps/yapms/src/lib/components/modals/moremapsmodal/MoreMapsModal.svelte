5cbc3032cb92c3a7bfafddf803c53dc7732dd578
Flizzyclone <stormry20@gmail.com>
2022-12-29 12:23:53 -0500

    Home page!

---
+++ apps/yapms/src/lib/components/modals/moremapsmodal/MoreMapsModal.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { MoreMapsModalStore } from '$lib/stores/HomeModals';
+	import mapList from '$lib/assets/other/MapList.json';
+
+	function close() {
+		MoreMapsModalStore.set({
+			...$MoreMapsModalStore,
+			open: false
+		});
+	}
+
+	//When the key updates, get the map data corresponding to that new key as defined in MapList.json. If none exists, empty data.
+	$: maps = mapList.find((elem) => elem.key === $MoreMapsModalStore.key) || {label:"",links:[]};
+</script>
+
+<input type="checkbox" class="modal-toggle" checked={$MoreMapsModalStore.open} />
+<div class="modal modal-bottom lg:modal-middle">
+	<div class="modal-box">
+		<h2 class="text-2xl">{maps.label} Maps</h2>
+		<br>
+		<div class="tabs flex-row lg:flex-col flex-end items-center space-y-2 justify-evenly">
+			{#each maps.links as link}
+			<a class="w-2/3 lg:w-1/2"href={link.route}>
+				<button on:click={close} class="btn btn-primary gap-1 w-full">
+					{link.label.toUpperCase()}
+				</button>
+			</a>
+			{/each}
+		</div>
+		<div class="modal-action">
+			<button class="btn btn-primary" on:click={close}>Close</button>
+		</div>
+	</div>
+</div>
--- apps/yapms/src/routes/+page.svelte
+++ apps/yapms/src/routes/+page.svelte
@@ -1,20 +1,42 @@
-<div class="navbar bg-base-200">
-	<h1 class="text-lg font-bold m-auto">Yet Another Political Map Simulator</h1>
-</div>
+<script>
+	import HomePageSidebar from "$lib/components/homePage/homePageSidebar.svelte";
+	import MapSelectionGrid from "$lib/components/homePage/mapSelectionGrid.svelte";
+	import MapSelectionTitle from "$lib/components/homePage/mapSelectionTitle.svelte";
+	import Login from '$lib/icons/Login.svelte';
+	import mapSelectSections from '$lib/assets/other/MapSelection.json';
+	import MapSelectionCard from "$lib/components/homePage/MapSelectionCard.svelte";
+	import MoreMapsModal from "$lib/components/modals/moremapsmodal/MoreMapsModal.svelte";
 
-<div class="grid grid-cols-2 gap-2 m-5">
-	<div class="card card-bordered w-96 bg-base-100 shadow-xl">
-		<div class="card-body">
-			<h2 class="card-title">United States</h2>
-			<div class="grid grid-cols-2 gap-2">
-				<a class="link" href="/app/usa/presidential/2022">President</a>
-				<a class="link" href="/app/usa/presidential/2022">Senate</a>
-				<a class="link" href="/app/usa/presidential/2022">House</a>
-				<a class="link" href="/app/usa/presidential/2022">Governor</a>
-			</div>
-			<div class="card-actions justify-end">
-				<button class="btn btn-sm btn-primary">Buy Now</button>
-			</div>
-		</div>
+</script>
+<div class="navbar bg-base-200 h-[10%]">
+	<div class="navbar-start"></div>
+	<div class="navbar-center">
+		<h1 class="text-2xl font-bold m-auto hidden lg:inline">Yet Another Political Map Simulator</h1>
+		<h1 class="text-2xl font-bold m-auto inline lg:hidden">YAPms</h1>
+	</div>
+	<div class="navbar-end">
+		<button class="btn px-8 btn-primary mr-2 hidden md:inline">Login</button> <!--needs to change into icon when screen small-->
+		<button class="btn btn-square mr-2 inline md:hidden"><Login class="h-8 m-auto"/></button>
 	</div>
 </div>
+
+<svelte:head>
+	<title>YAPms - Yet Another Political Map Simulator</title>
+</svelte:head>
+
+<div class="flex flex-row w-full h-[90%]">
+	<HomePageSidebar/>
+	<div class="divider md:divider-horizontal ml-0 w-0" />
+	<div class="flex-1 md:pr-5 overflow-y-auto overflow-x-clip">
+		{#each mapSelectSections as section}
+			<MapSelectionTitle title={section.title}></MapSelectionTitle>
+			<MapSelectionGrid>
+				{#each section.cards as card}
+					<MapSelectionCard name={card.name} bg={card.bg} alt={card.alt} doubleCols={card.doubleCols} links={card.links}></MapSelectionCard>
+				{/each}
+			</MapSelectionGrid>
+		{/each}
+	</div>
+</div>
+
+<MoreMapsModal/>
