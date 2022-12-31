1aafcba1e6b094bc6bcef0b65cd533af6c141811
Jacob Enders <jacobenders1@gmail.com>
2022-12-30 21:37:47 -0500

    fix linting

--- apps/yapms/src/lib/stores/PocketBase.ts
+++ apps/yapms/src/lib/stores/PocketBase.ts
@@ -2,6 +2,4 @@ import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 import { PUBLIC_POCKETBASE_URI } from '$env/static/public';
 
-export const PocketBaseStore = writable(
-  new PocketBase(PUBLIC_POCKETBASE_URI)
-);
+export const PocketBaseStore = writable(new PocketBase(PUBLIC_POCKETBASE_URI));
