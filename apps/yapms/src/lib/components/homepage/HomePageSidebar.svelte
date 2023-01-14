3b6b4c4ee1d072a260d6b9f6f6f9271f3ae0e34c
Flizzyclone <stormry20@gmail.com>
2023-01-14 00:19:02 -0500

    Updates from pocketbase

--- apps/yapms/src/lib/components/homepage/HomePageSidebar.svelte
+++ apps/yapms/src/lib/components/homepage/HomePageSidebar.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
 	import Fa from 'svelte-fa';
 	import { faDiscord, faGithub, faReddit, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+	import { onMount } from "svelte";
+	import { PocketBaseStore } from '$lib/stores/PocketBase';
+	import SideBarUpdate from './SideBarUpdate.svelte';
+	import type { Record } from 'pocketbase';
+
+	let updates:Record[] = [];
+
+	onMount(async () => {
+		//Request the last 10 update records from pocketbase.
+		$PocketBaseStore.collection('updates').getList(1, 10, {sort: '-created'})
+		.then(data => {
+			updates = data.items
+		}).catch(error => {
+			console.log(error);
+		});
+	});
 </script>
 
 <div class="lg:w-1/5 pl-5 hidden md:flex flex-col h-full justify-between">
-	<div>
+	<div class="overflow-y-auto">
 		<div class="divider">Updates</div>
-		<p>This will eventually be updated by a server</p>
+		{#each updates as update}
+			<SideBarUpdate title={update.title} description={update.description}></SideBarUpdate>
+		{/each}
 	</div>
 	<div class="mb-4">
 		<div class="divider">Social Links</div>
---
+++ apps/yapms/src/lib/components/homepage/SideBarUpdate.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let title: string
+    export let description: string
+</script>
+
+<div class="card card-bordered bg-base-300 shadow-md p-3 mb-3">
+    <div class="text-xl text-center md:text-left font-semibold pb-2">
+        {title}
+    </div>
+    <div class="text-md text-center md:text-left">
+        {description}
+    </div>
+</div>
