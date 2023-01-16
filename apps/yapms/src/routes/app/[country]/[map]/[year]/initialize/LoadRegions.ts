5ab15cd391780738815bd8af80c1910f5582b6c7
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2023-01-15 21:41:19 -0500

    Decrement states using Ctrl (#98)
    * Decrement states using ctrl
    * Double ternary -> if + ternary

--- apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/LoadRegions.ts
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/LoadRegions.ts
@@ -15,6 +15,7 @@ function fillRegion(regionID: string, increment: boolean) {
 	const regions = get(RegionsStore);
 	const region = regions.find((region) => region.id === regionID);
 	if (region && !(region.disabled || region.locked)) {
+		const currentInteractions = get(InteractionStore);
 		const selectedCandidate = get(SelectedCandidateStore);
 		const currentCandidate = region.candidates[0];
 		const newCandidate = {
@@ -23,10 +24,19 @@ function fillRegion(regionID: string, increment: boolean) {
 			margin: 0
 		};
 		if (currentCandidate.candidate.id === selectedCandidate.id && increment) {
-			newCandidate.margin =
-				currentCandidate.margin + 1 >= selectedCandidate.margins.length
-					? 0
-					: currentCandidate.margin + 1;
+			if (!currentInteractions.has('ControlLeft')) {
+				//Increment
+				newCandidate.margin =
+					currentCandidate.margin + 1 >= selectedCandidate.margins.length
+						? 0
+						: currentCandidate.margin + 1;
+			} else {
+				//Decrement
+				newCandidate.margin =
+					currentCandidate.margin - 1 <= -1
+						? selectedCandidate.margins.length - 1
+						: currentCandidate.margin - 1;
+			}
 		}
 		region.candidates = [newCandidate];
 		RegionsStore.set(regions);
