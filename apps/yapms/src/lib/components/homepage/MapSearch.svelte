ab6394f9da1e597aa64998e72d2dcda70d2eabff
Flizzyclone <stormry20@gmail.com>
2023-03-28 22:30:08 -0400

    Changed to Magnifying Glass

--- apps/yapms/src/lib/components/homepage/MapSearch.svelte
+++ apps/yapms/src/lib/components/homepage/MapSearch.svelte
@@ -2,7 +2,7 @@
 	import titles from '$lib/assets/other/Titles.json';
 	import Typeahead from 'svelte-typeahead';
 	import { goto } from '$app/navigation';
-	import Search from '$lib/icons/Search.svelte';
+	import MagnifyingGlass from '$lib/icons/MagnifyingGlass.svelte';
 
 	function onSelect({ detail }: CustomEvent) {
 		void goto(titles[detail.originalIndex].path);
@@ -11,14 +11,7 @@
 
 <div class="pt-6">
 	<div class="px-6 md:px-0 w-full lg:w-4/6 flex">
-		<Search class={`pointer-events-none stroke-current my-3 pt-1 w-9 z-10 ml-2`}
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-			/></Search
-		>
+		<MagnifyingGlass class={`pointer-events-none stroke-current my-3 pt-1 w-9 z-10 ml-2`} />
 		<Typeahead
 			placeholder={'Search Maps'}
 			limit={8}
