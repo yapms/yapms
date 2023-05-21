c3e9d5a5eda18ab46f55000d95f1777e6605d5c0
Jacob Enders <>
2023-05-21 19:44:30 -0400

    add embed data to /app page

--- apps/yapms/src/routes/app/+page.svelte
+++ apps/yapms/src/routes/app/+page.svelte
@@ -1,8 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { PUBLIC_POCKETBASE_URI } from '$env/static/public';
+
+	const url = $page.url;
+	const m = url.searchParams.get('m');
+	const imageURI =
+		m !== undefined ? `${PUBLIC_POCKETBASE_URI}/api/files/maps/${m}/screenshot.png` : '';
+</script>
+
 <svelte:head>
-	<title>YAPms</title>
-	<meta property="og:title" content="YAPms" />
+	<title>YAPms - Yet Another Political Map Simulator</title>
+	<meta property="og:site_name" content="YAPms" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="User Generated Map" />
 	<meta property="og:description" content="Yet Another Political Map Simulator" />
-	<meta property="twitter:description" content="Yet Another Political Map Simulator" />
+	<meta property="og:image" content={imageURI} />
+	<meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
 <div>Please wait while we redirect you to the map you requested...</div>
