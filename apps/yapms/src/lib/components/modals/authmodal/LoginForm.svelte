67482f7c6e9c3b0885c28c7c3746ae382e9e593e
Jacob Enders <jacobenders1@gmail.com>
2023-06-08 19:24:15 -0400

    Update Pocketbase Authentication (#168)

--- apps/yapms/src/lib/components/modals/authmodal/LoginForm.svelte
+++ apps/yapms/src/lib/components/modals/authmodal/LoginForm.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
 	import { PocketBaseStore } from '$lib/stores/PocketBase';
-	import { PUBLIC_REDIRECT_URI } from '$env/static/public';
-	import { page } from '$app/stores';
 
 	const authMethods = $PocketBaseStore.collection('users').listAuthMethods();
 
-	function authenticate(authUrl: string, provider: string, codeVerifier: string, state: string) {
-		localStorage.setItem('codeVerifier', codeVerifier);
-		localStorage.setItem('state', state);
-		localStorage.setItem('provider', provider);
-		localStorage.setItem('redirect', $page.url.href);
-		const url = new URL(authUrl + PUBLIC_REDIRECT_URI);
-		window.location.href = url.href;
+	async function authenticate(provider: string) {
+		await $PocketBaseStore.collection('users').authWithOAuth2({
+			provider
+		});
+		PocketBaseStore.set($PocketBaseStore);
 	}
 </script>
 
@@ -22,8 +18,8 @@
 		{#each authMethods.authProviders as method}
 			<button
 				class="btn btn-accent"
-				on:click={() => {
-					authenticate(method.authUrl, method.name, method.codeVerifier, method.state);
+				on:click={async () => {
+					await authenticate(method.name);
 				}}
 			>
 				Login With {method.name}
