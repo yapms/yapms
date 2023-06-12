851392b96b8afdc5b02c0357600147901fb6c50e
Jacob Enders <jacobenders1@gmail.com>
2023-06-12 19:11:17 -0400

    Umami Analytics (#174)

--- apps/yapms/src/routes/+layout.svelte
+++ apps/yapms/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '$lib/styles/global.css';
+	import { PUBLIC_UMAMI_URI, PUBLIC_UMAMI_DATA_WEBSITE_ID } from '$env/static/public';
 </script>
+
+<svelte:head>
+	<script async src={PUBLIC_UMAMI_URI} data-website-id={PUBLIC_UMAMI_DATA_WEBSITE_ID}></script>
+</svelte:head>
 
 <slot />
