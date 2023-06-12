2c50f2b70e7c16fc3067ba4abd62e3caa4de5734
Flizzyclone <stormry20@gmail.com>
2023-06-12 16:42:53 -0400

    Hooking up imported stroke scaling

--- apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/ApplyPanZoom.ts
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/initialize/ApplyPanZoom.ts
@@ -3,13 +3,18 @@ import panzoom, { type PanZoom } from 'panzoom';
 function applyPanZoom(mapBind: HTMLDivElement): PanZoom | undefined {
 	const svg = mapBind.querySelector<SVGElement>('svg'); //We can do this since the only child of node is the map svg itself.
 	if (svg) {
-		return panzoom(svg, {
+		const panzoomInstance = panzoom(svg, {
 			minZoom: 0.5,
 			maxZoom: 100,
 			smoothScroll: false,
 			autocenter: true,
 			zoomDoubleClickSpeed: 1
 		});
+		panzoomInstance.on('zoom', function (e: PanZoom) {
+			const scale = e.getTransform().scale;
+			svg?.style.setProperty('--imported-stroke-width', `${0.5 / scale}px`);
+		});
+		return panzoomInstance
 	}
 }
 
