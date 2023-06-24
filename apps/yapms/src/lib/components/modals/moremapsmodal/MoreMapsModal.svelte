fb4918691c04e965c1e02ba1ff12971b8624be7f
Flizzyclone <stormry20@gmail.com>
2023-06-24 11:02:59 -0400

    Reversion of MoreMapsModal changes

--- apps/yapms/src/lib/components/modals/moremapsmodal/MoreMapsModal.svelte
+++ apps/yapms/src/lib/components/modals/moremapsmodal/MoreMapsModal.svelte
@@ -1,34 +1,33 @@
 <script lang="ts">
 	import { MoreMapsModalStore } from '$lib/stores/HomeModals';
-	import titles from '$lib/assets/other/Titles.json';
-	import ModalBase from '../ModalBase.svelte';
-
+	import mapList from '$lib/assets/homedata/MapList.json';
 	function close() {
 		MoreMapsModalStore.set({
 			...$MoreMapsModalStore,
 			open: false
 		});
 	}
-
-	//When the key updates, get the map data corresponding to that new key as defined in titles.json. If none exists, empty data.
-	$: maps = titles.filter((elem) => elem.path.startsWith(`/app/${$MoreMapsModalStore.key}`)) || [];
-	$: namespace = titles.find((elem) => elem.path === $MoreMapsModalStore.key);
+	//When the key updates, get the map data corresponding to that new key as defined in MapList.json. If none exists, empty data.
+	$: maps = mapList.find((elem) => elem.key === $MoreMapsModalStore.key) || {
+		label: '',
+		links: []
+	};
 </script>
 
-<ModalBase
-	title="{namespace !== undefined ? namespace.title : ''} Maps"
-	open={$MoreMapsModalStore.open}
->
-	<div slot="content">
+<input type="checkbox" class="modal-toggle" checked={$MoreMapsModalStore.open} />
+<div class="modal modal-bottom lg:modal-middle">
+	<div class="modal-box">
+		<h2 class="text-2xl">{maps.label} Maps</h2>
+		<br />
 		<div class="tabs flex-row lg:flex-col flex-end items-center space-y-2 justify-evenly">
-			{#each maps as map}
-				<a class="w-2/3 lg:w-1/2 btn btn-primary" href={map.path} on:click={close}>
-					{map.title}
+			{#each maps.links as link}
+				<a class="w-2/3 lg:w-1/2 btn btn-primary gap-1" href={link.route} on:click={close}>
+					{link.label.toUpperCase()}
 				</a>
 			{/each}
 		</div>
+		<div class="modal-action">
+			<button class="btn btn-primary" on:click={close}>Close</button>
+		</div>
 	</div>
-	<div slot="action">
-		<button class="btn btn-primary" on:click={close}>Close</button>
-	</div>
-</ModalBase>
+</div>
