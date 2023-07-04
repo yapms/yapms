86948839827630bc4b4b793f617954fb29a7592e
Jacob Enders <>
2023-07-03 22:17:17 -0400

    change to zod string
    With zod strings we can ensure that only approved characters go into the URL, and we don't need to manage an enum list.

--- apps/yapms/src/lib/types/SavedMap.ts
+++ apps/yapms/src/lib/types/SavedMap.ts
@@ -2,17 +2,11 @@ import { z } from 'zod';
 import { CandidateSchema } from './Candidate';
 import { SavedRegionSchema } from './Region';
 
-const countries = ['usa'] as const;
-
-const types = ['presidential'] as const;
-
-const years = ['2022'] as const;
-
 export const SavedMapSchema = z.object({
 	map: z.object({
-		country: z.enum(countries),
-		type: z.enum(types),
-		year: z.enum(years)
+		country: z.string().regex(/^[a-z]+$/),
+		type: z.string().regex(/^[a-z]+$/),
+		year: z.string().regex(/^\d{4}$/)
 	}),
 	tossup: CandidateSchema,
 	candidates: CandidateSchema.array(),
