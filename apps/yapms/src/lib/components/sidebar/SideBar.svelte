5aa2dbec630f6a76fa983513b0e03af048e808d5
Jacob Enders <>
2023-07-06 20:31:40 -0400

    undo social media

--- apps/yapms/src/lib/components/sidebar/SideBar.svelte
+++ apps/yapms/src/lib/components/sidebar/SideBar.svelte
@@ -3,7 +3,8 @@
 	import titles from '$lib/assets/other/Titles.json';
 	import { SideBarStore } from '$lib/stores/SideBar';
 	import SavedMaps from './sections/savedmaps/SavedMaps.svelte';
-	import SocialMedia from './sections/SocialMedia.svelte';
+	import { faReddit, faTwitter } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
 
 	$: title = titles.find((elem) => elem.path === $page.url.pathname)?.title ?? 'YAPms';
 </script>
@@ -11,7 +12,16 @@
 {#if $SideBarStore}
 	<div class="divider divider-horizontal w-0 m-0 hidden md:flex" />
 	<div class="basis-3/12 max-w-md hidden md:inline overflow-hidden">
-		<SocialMedia />
+		<div class="flex flex-wrap justify-center gap-2 p-2">
+			<button type="button" class="btn btn-sm gap-2">
+				<Fa icon={faReddit} />
+				Reddit
+			</button>
+			<button type="button" class="btn btn-sm gap-2">
+				<Fa icon={faTwitter} />
+				Twitter
+			</button>
+		</div>
 		<h1 class="text-xl text-center font-bold">{title}</h1>
 		<div class="divider">Shortcuts</div>
 		<SavedMaps />
