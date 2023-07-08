3690d891bb79f9acbacfac6d90fe4a0dbdb38893
Jacob Enders <>
2023-07-07 20:32:25 -0400

    remove console log

--- apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
+++ apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
@@ -15,8 +15,6 @@
 	async function createMap() {
 		submitting = true;
 		const form = new FormData();
-		const json = generateJson();
-		console.log(json);
 		const newMapData = new File([JSON.stringify(generateJson())], 'data.json', {
 			type: 'application/json'
 		});
