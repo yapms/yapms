a84f8ad04a2a64f6b09f332a5100c5d7242097b7
Flizzyclone <stormry20@gmail.com>
2023-07-13 11:23:28 -0400

    refactor to use .split

--- apps/yapms/src/routes/app/[country]/[map]/[year]/+page.server.ts
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/+page.server.ts
@@ -3,16 +3,11 @@ import titles from '$lib/assets/other/Titles.json';
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
 	return titles.map((title) => {
-		let path = title.path;
-		path = path.replace('/app/', '');
-		const country = path.match(/.+?(?=\/)/)?.[0];
-		path = path.replace(`${country}/`, '');
-		const map = path.match(/.+?(?=\/)/)?.[0];
-		const year = path.replace(`${map}/`, '');
-		if (country === undefined || map === undefined) {
+		const params = title.path.split('/');
+		if (params[2] === undefined || params[3] === undefined || params[4] === undefined) {
 			return { country: 'usa', map: 'presidential', year: '2022' };
 		}
-		return { country, map, year };
+		return { country: params[2], map: params[3], year: params[4] };
 	});
 }
 
