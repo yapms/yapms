3c6b635629560cef11ac9e7adf3532122a263ab7
Flizzyclone <stormry20@gmail.com>
2023-07-13 10:47:09 -0400

    Tell Svelte which routes to render

--- apps/yapms/src/routes/app/[country]/[map]/[year]/+page.server.ts
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/+page.server.ts
@@ -1,1 +1,19 @@
+import titles from '$lib/assets/other/Titles.json';
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return titles.map((title) => {
+		let path = title.path;
+		path = path.replace('/app/', '');
+		const country = path.match(/.+?(?=\/)/)?.[0];
+		path = path.replace(`${country}/`, '');
+		const map = path.match(/.+?(?=\/)/)?.[0];
+		const year = path.replace(`${map}/`, '');
+		if (country === undefined || map === undefined) {
+			return { country: 'usa', map: 'presidential', year: '2022' };
+		}
+		return { country, map, year };
+	});
+}
+
 export const prerender = true;
