05eb2f7cc3d03ea5b207173d522c75560b11c01b
Jacob Enders <jacobenders1@gmail.com>
2023-07-21 21:38:25 -0400

    remove functions from routes (#248)

--- apps/yapms/src/routes/app/[country]/[map]/[year]/+page.svelte
+++ apps/yapms/src/routes/app/[country]/[map]/[year]/+page.svelte
@@ -1,60 +1,63 @@
 <script lang="ts">
-	import applyPanZoom from './initialize/ApplyPanZoom';
 	import { loadFromJson } from '$lib/utils/loadMap';
 	import { page } from '$app/stores';
-	import { loadRegionsForApp } from './initialize/LoadRegions';
 	import { MapInsetsStore } from '$lib/stores/MapInsetsStore';
 	import { LoadedMapStore } from '$lib/stores/LoadedMap';
+	import applyPanZoom from '$lib/utils/applyPanZoom';
+	import { loadRegionsForApp } from '$lib/utils/loadRegions';
+	import { goto } from '$app/navigation';
+	import { browser } from '$app/environment';
 
-	//Glob import all maps in the maps directory so that we can check if a map exists and then load it.
-	//Query section makes sure the SVG contents are imported raw.
-	const imports = import.meta.glob<typeof import('*?raw')>('$lib/assets/maps/*/*.svg', {
-		//Import from subdirectories
+	type ImportedFile = typeof import('*?raw');
+	type ImportedFilePromise = () => Promise<ImportedFile>;
+
+	// import all files from $lib/assets/maps/
+	const importedFiles = import.meta.glob<ImportedFile>('$lib/assets/maps/*/*.svg', {
 		query: { raw: '' }
 	});
 
-	//Strip directory information from import keys ("/src/lib/assets/maps/usa/usa-governors-2025.svg" to "usa-governors-2025.svg")
-	for (const path in imports) {
-		const fileName = path.split('/').pop();
-		if (fileName !== undefined) {
-			imports[fileName] = imports[path];
+	const maps = new Map<string, ImportedFilePromise>();
+
+	for (const key in importedFiles) {
+		const filename = key.split('/').pop();
+		const promise = importedFiles[key];
+		if (filename !== undefined) {
+			maps.set(filename, promise);
 		}
-		delete imports[path];
 	}
 
-	$: requestedMap = $page.url.pathname.replace('/app/', '').replaceAll('/', '-').concat('.svg');
+	const requestedMap = $page.url.pathname.replace('/app/', '').replaceAll('/', '-').concat('.svg');
 
-	$: currentMap = imports[requestedMap] !== undefined ? requestedMap : 'usa-presidential-2022.svg';
+	const currentMap = maps.get(requestedMap) ?? maps.get('usa-presidential-2022.svg');
+
+	if (browser && maps.has(requestedMap) === false) {
+		goto('/app/usa/presidential/2022', { replaceState: true });
+	}
 
 	let isLoaded = false;
-
-	// this should execute if the user enters
-	// this page with a map ID
-	$: if ($LoadedMapStore !== null && isLoaded) {
-		loadFromJson($LoadedMapStore);
-	}
 
 	function setupMap(node: HTMLDivElement) {
 		applyPanZoom(node);
 		loadRegionsForApp(node);
 		isLoaded = true;
-		// this should execute if the users enters the
-		// /app/ page with a map id
-		if ($LoadedMapStore !== null) {
-			loadFromJson($LoadedMapStore);
-		}
+	}
+
+	$: if ($LoadedMapStore !== null && isLoaded) {
+		loadFromJson($LoadedMapStore);
 	}
 </script>
 
-{#await imports[currentMap]()}
-	<h1>Loading Map...</h1>
-{:then importedMap}
-	<div
-		use:setupMap
-		id="map-div"
-		class="overflow-hidden h-full"
-		class:insets-hidden={$MapInsetsStore.hidden}
-	>
-		{@html importedMap.default}
-	</div>
-{/await}
+{#if currentMap !== undefined}
+	{#await currentMap()}
+		<h1>Loading Map...</h1>
+	{:then importedMap}
+		<div
+			use:setupMap
+			id="map-div"
+			class="overflow-hidden h-full"
+			class:insets-hidden={$MapInsetsStore.hidden}
+		>
+			{@html importedMap.default}
+		</div>
+	{/await}
+{/if}
