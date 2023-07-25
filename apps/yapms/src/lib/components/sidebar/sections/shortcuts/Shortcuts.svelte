b60a2451425d911a70e44c5919a859311fd880a7
Jacob Enders <>
2023-07-24 22:27:37 -0400

    shortcuts documented on sidebar

---
+++ apps/yapms/src/lib/components/sidebar/sections/shortcuts/Shortcuts.svelte
@@ -0,0 +1,11 @@
+<div class="divider">Shortcuts</div>
+<div class="flex flex-col gap-4 p-2">
+	<div class="flex items-center gap-2">
+		<kbd class="kbd">f</kbd>
+		<span>hold down to quickly fill in regions</span>
+	</div>
+	<div class="flex items-center gap-2">
+		<kbd class="kbd">ctrl</kbd>
+		<span>hold down to decrement colors</span>
+	</div>
+</div>
