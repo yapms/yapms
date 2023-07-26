c50e5d741d7798e6bf07ffd075671ecd5d14058a
Jacob Enders <>
2023-07-25 22:58:47 -0400

    show the f as lowercase in the shortcut

--- apps/yapms/src/lib/components/sidebar/sections/shortcuts/Shortcuts.svelte
+++ apps/yapms/src/lib/components/sidebar/sections/shortcuts/Shortcuts.svelte
@@ -2,7 +2,7 @@
 <div class="flex flex-col gap-4 p-2">
 	<div class="flex justify-center items-center gap-2">
 		<span>Hold</span>
-		<kbd class="kbd">F</kbd>
+		<kbd class="kbd">f</kbd>
 		<span>to quickly fill in regions</span>
 	</div>
 	<div class="flex justify-center items-center gap-2">
