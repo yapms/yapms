888c92a95ac1c53c51cc60b2bf698d0b02ddfef9
Jacob Enders <>
2023-08-03 22:55:34 -0400

    init

--- apps/yapms/src/lib/components/modals/ModalBase.svelte
+++ apps/yapms/src/lib/components/modals/ModalBase.svelte
@@ -1,37 +1,31 @@
 <script lang="ts">
 	export let open: boolean;
 	export let title: string;
-
-	export let sticky = false;
 </script>
 
 <input type="checkbox" class="modal-toggle" checked={open} />
 
 <div class="modal modal-bottom lg:modal-middle">
-	<div class="modal-box p-0 flex flex-col">
-		<div class="bg-base-100 pt-6" class:border-b={sticky} class:border-base-content={sticky}>
+	<div class="modal-box flex flex-col">
+		<div class="bg-base-100">
 			<div class="flex gap-x-2 ml-6">
 				<slot name="icon" />
-				<h3 class="text-2xl pb-3">
+				<h3 class="font-bold text-xl">
 					{title}
 				</h3>
 			</div>
 		</div>
 
-		<div class="px-6 flex-1" class:overflow-y-auto={sticky}>
+		<div class="divider mb-0" />
+
+		<div class="overflow-y-auto">
 			<slot name="content" />
 		</div>
 
-		<div
-			class="modal-action bg-base-100 pb-6"
-			class:mt-0={sticky}
-			class:pt-3={sticky}
-			class:border-t={sticky}
-			class:border-base-content={sticky}
-		>
-			<div class="mr-6">
-				<slot name="action" />
-			</div>
+		<div class="divider mt-0" />
+
+		<div class="modal-action mt-0">
+			<slot name="action" />
 		</div>
 	</div>
 </div>
