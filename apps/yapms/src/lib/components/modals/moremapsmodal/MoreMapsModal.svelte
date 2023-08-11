fa6952b4589366b390e4f7059e2a3c3eead9d958
Jacob Enders <>
2023-08-10 22:57:48 -0400

    don't close modal on ctrl click

--- apps/yapms/src/lib/components/modals/moremapsmodal/MoreMapsModal.svelte
+++ apps/yapms/src/lib/components/modals/moremapsmodal/MoreMapsModal.svelte
@@ -2,8 +2,8 @@
 	import { MoreMapsModalStore } from '$lib/stores/HomeModals';
 	import ModalBase from '../ModalBase.svelte';
 
-	function close() {
-		$MoreMapsModalStore.open = false;
+	function close(event: MouseEvent) {
+		if (!event.ctrlKey) $MoreMapsModalStore.open = false;
 	}
 </script>
 
@@ -12,7 +12,7 @@
 		<div class="tabs flex-row lg:flex-col flex-end items-center space-y-2 justify-evenly my-3">
 			{#each $MoreMapsModalStore.buttons as button}
 				<a class="w-2/3 lg:w-1/2 btn btn-primary gap-1" href={button.route} on:click={close}>
-					{button.label.toUpperCase()}
+					{button.label}
 				</a>
 			{/each}
 		</div>
