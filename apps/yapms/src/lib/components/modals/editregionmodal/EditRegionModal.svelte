a8aeccca287b2eea060cf53b711e31ea9066ffc7
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2023-08-13 22:52:49 -0400

    Use forms for text input (#295)

--- apps/yapms/src/lib/components/modals/editregionmodal/EditRegionModal.svelte
+++ apps/yapms/src/lib/components/modals/editregionmodal/EditRegionModal.svelte
@@ -8,11 +8,14 @@
 	$: value = open ? $EditRegionModalStore.region?.permaVal : undefined;
 	$: newValue = value ?? 0;
 
-	$: if (newValue < 0 || newValue === null) {
-		newValue = 0;
+	function preventNonNumericalInput(e: KeyboardEvent) {
+		if (!e.key.match(/^[0-9]+$/)) e.preventDefault();
 	}
 
 	function confirm() {
+		if (newValue === null) {
+			newValue = 0;
+		}
 		const index = $RegionsStore.findIndex(
 			(region) => region.id === $EditRegionModalStore.region?.id
 		);
@@ -27,14 +30,17 @@
 
 <ModalBase title="Edit Region {longName} - {value}" store={EditRegionModalStore}>
 	<div slot="content">
-		<input
-			type="number"
-			placeholder="Value"
-			class="input input-bordered w-full"
-			min="0"
-			bind:value={newValue}
-			required
-		/>
+		<div class="form-control w-full flex flex-col gap-3">
+			<h3 class="font-light text-lg">Region Value</h3>
+			<form on:submit={confirm}>
+				<input
+					type="number"
+					class="input input-bordered w-full"
+					on:keypress={preventNonNumericalInput}
+					bind:value={newValue}
+				/>
+			</form>
+		</div>
 	</div>
 	<div slot="action">
 		<button class="btn btn-success" on:click={confirm}>Confirm</button>
