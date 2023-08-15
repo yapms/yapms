ebd40c7e5728ffac01f8760884103e874996af23
Jacob Enders <jacobenders1@gmail.com>
2023-08-15 18:15:50 -0400

    Fix theme not changing bug (#305)

--- apps/yapms/src/lib/components/modals/thememodal/ThemeModal.svelte
+++ apps/yapms/src/lib/components/modals/thememodal/ThemeModal.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { ThemeModalStore } from '$lib/stores/Modals';
-	import ThemeGrid from '../../modalutilities/ThemeGrid.svelte';
+	import ThemeGrid from './ThemeGrid.svelte';
 	import ModalBase from '../ModalBase.svelte';
 </script>
 
