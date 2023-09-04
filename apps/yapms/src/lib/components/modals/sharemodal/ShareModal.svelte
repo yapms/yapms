c858569376f203ee7d1ec67a3f1daed78bc85d9f
Jacob Enders <>
2023-09-04 17:42:15 -0400

    added tooltip

--- apps/yapms/src/lib/components/modals/sharemodal/ShareModal.svelte
+++ apps/yapms/src/lib/components/modals/sharemodal/ShareModal.svelte
@@ -110,16 +110,22 @@
 					<ArrowDownTray class="w-5 h-5" />
 					<span>Download</span>
 				</button>
-				<button
-					class="btn btn-secondary gap-1 flex-nowrap"
-					on:click={generateLink}
-					disabled={fetchingLink ||
-						turnstileToken === null ||
-						$page.url.pathname === '/app/imported'}
+				<div
+					class="tooltip-right"
+					class:tooltip={$page.url.pathname === '/app/imported'}
+					data-tip="Can not link to an imported map"
 				>
-					<Link class="w-5 h-5" />
-					<span>Generate Link</span>
-				</button>
+					<button
+						class="btn btn-secondary gap-1 flex-nowrap"
+						on:click={generateLink}
+						disabled={fetchingLink ||
+							turnstileToken === null ||
+							$page.url.pathname === '/app/imported'}
+					>
+						<Link class="w-5 h-5" />
+						<span>Generate Link</span>
+					</button>
+				</div>
 			</div>
 
 			<div class="divider divider-horizontal" />
