d6ce8e439cafd7a039770e2824fbda5e8a0f7cd4
Jacob Enders <>
2023-09-06 23:32:36 -0400

    update type errors

--- apps/yapms/src/lib/utils/importMap.ts
+++ apps/yapms/src/lib/utils/importMap.ts
@@ -35,12 +35,12 @@ export const DOMPurifyConfig = {
 };
 
 async function importFromShapefiles(files: FileList): Promise<void> {
-	const districtShapes = await shapefile.read(files[0].stream());
+	const districtShapes = await shapefile.read(files[0].stream().read());
 	districtShapes.features = [];
 
 	const unresolvedFeatures = [];
 	for (const file of files) {
-		unresolvedFeatures.push(shapefile.read(file.stream()));
+		unresolvedFeatures.push(shapefile.read(file.stream().read()));
 	}
 
 	const resolvedFeatures = await Promise.all(unresolvedFeatures);
