1de410086d5a97dc13d10ecb98c3f74e1eb4d2a1
Jacob Enders <>
2023-09-14 23:35:20 -0400

    init

---
+++ apps/yapms/src/lib/icons/Bars3.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let className = '';
+	export { className as class };
+	export let style = '';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class={className}
+	{style}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+	/>
+</svg>
