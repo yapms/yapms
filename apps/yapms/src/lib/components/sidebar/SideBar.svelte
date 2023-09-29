de3c12c9d223a11a103af10aba25530ab1b483fd
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2023-09-28 21:54:27 -0400

    Side bar on mobile (#383)

--- apps/yapms/src/lib/components/sidebar/SideBar.svelte
+++ apps/yapms/src/lib/components/sidebar/SideBar.svelte
@@ -16,16 +16,18 @@
 </script>
 
 {#if $SideBarStore}
-	<div class="divider divider-horizontal w-0 m-0 hidden md:flex" />
-	<div class="basis-3/12 max-w-md hidden md:inline overflow-y-auto">
-		<div class="flex flex-wrap justify-center p-2">
-			<SocialLinkGrid />
+	<div class="z-10 h-full flex absolute right-0 md:relative md:basis-3/12 max-w-lg">
+		<div class="divider divider-horizontal w-0 m-0 flex" />
+		<div class="overflow-y-auto bg-base-100 w-full px-4 md:px-0">
+			<div class="flex flex-wrap justify-center p-2">
+				<SocialLinkGrid />
+			</div>
+			<h1 class="text-xl text-center font-bold">{title}</h1>
+			<Shortcuts />
+			{#if $PocketBaseStore.authStore.isValid}
+				<SavedMaps />
+			{/if}
+			<Sources />
 		</div>
-		<h1 class="text-xl text-center font-bold">{title}</h1>
-		<Shortcuts />
-		{#if $PocketBaseStore.authStore.isValid}
-			<SavedMaps />
-		{/if}
-		<Sources />
 	</div>
 {/if}
--- apps/yapms/src/lib/stores/SideBar.ts
+++ apps/yapms/src/lib/stores/SideBar.ts
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 
-export const SideBarStore = writable(true);
+export const SideBarStore = writable(false);
