e0b860f9630be95373f041cd971a3c3d4c62c463
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2023-10-29 17:50:50 -0400

    Brazilian maps (#419)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/BRAMapCard.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Chamber of Deputies',
+			route: '/app/bra/deputies/2022/blank'
+		},
+		{
+			label: 'Senate',
+			route: '/app/bra/senate/2022/blank'
+		},
+		{
+			label: 'Federative Units',
+			route: '/app/bra/regions/2023/blank'
+		},
+		{
+			label: 'Chamber of Deputies 2022',
+			route: '/app/bra/deputies/2022/blank'
+		},
+		{
+			label: 'Senate 2022',
+			route: '/app/bra/senate/2022/blank'
+		},
+		{
+			label: 'Senate',
+			route: '/app/bra/senate'
+		},
+		{
+			label: 'Federative Units',
+			route: '/app/bra/regions/2023/blank'
+		}
+	];
+</script>
+
+<MapCard name="Brazil" bg="bra" attribution="Photo by Magali Guimarães" {links} />
