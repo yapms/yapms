bd9ef070f1b48e18af51b575d474d48291f89998
Jacob Enders <jacobenders1@gmail.com>
2023-10-29 20:59:22 -0400

    Increase size of save file to 0.5 MB (#426)
    Co-authored-by: Jacob Enders <>

---
+++ apps/pocketbase/migrations/1698615826_updated_user_maps.go
@@ -0,0 +1,78 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("et0wg1x7mdn55wc")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_data := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "c4cbfhux",
+			"name": "data",
+			"type": "file",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"maxSelect": 1,
+				"maxSize": 500000,
+				"mimeTypes": [
+					"application/gzip",
+					"application/json"
+				],
+				"thumbs": [],
+				"protected": false
+			}
+		}`), edit_data)
+		collection.Schema.AddField(edit_data)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("et0wg1x7mdn55wc")
+		if err != nil {
+			return err
+		}
+
+		// update
+		edit_data := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "c4cbfhux",
+			"name": "data",
+			"type": "file",
+			"required": true,
+			"presentable": false,
+			"unique": false,
+			"options": {
+				"maxSelect": 1,
+				"maxSize": 250000,
+				"mimeTypes": [
+					"application/gzip",
+					"application/json"
+				],
+				"thumbs": [],
+				"protected": false
+			}
+		}`), edit_data)
+		collection.Schema.AddField(edit_data)
+
+		return dao.SaveCollection(collection)
+	})
+}
