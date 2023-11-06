4121fc43f160384a5ab552de919d7eee78a2f7ae
Jacob Enders <jacobenders1@gmail.com>
2023-11-05 19:39:04 -0500

    Disable the save button on imported maps. (#460)

--- apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
+++ apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
@@ -2,6 +2,7 @@
 	import { PocketBaseStore } from '$lib/stores/PocketBase';
 	import { generateJson } from '$lib/utils/saveMap';
 	import PlusCircle from '$lib/icons/PlusCircle.svelte';
+	import { page } from '$app/stores';
 
 	export let disabled = false;
 	export let onSubmitted: () => void;
@@ -29,6 +30,7 @@
 		newMapName = '';
 		onSubmitted();
 	}
+	console.log($page.url.pathname.split('/').at(2));
 </script>
 
 <div class="join">
@@ -37,13 +39,13 @@
 		class="input input-bordered input-sm flex-grow overflow-hidden join-item"
 		bind:value={newMapName}
 		placeholder="Map Name"
-		disabled={disabled || submitting}
+		disabled={disabled || submitting || $page.url.pathname === '/app/imported'}
 	/>
 	<div class="tooltip" data-tip="Create">
 		<button
 			class="btn btn-sm btn-primary join-item"
 			on:click={createMap}
-			disabled={disabled || submitting}
+			disabled={disabled || submitting || $page.url.pathname === '/app/imported'}
 		>
 			{#if submitting}
 				<span class="loading loading-dots loading-sm" />
