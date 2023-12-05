a9064098f05d664bbafe1ea82b0bd1f3d9b14c1f
Luvexina <44413950+VFDan@users.noreply.github.com>
2023-12-05 00:22:05 -0500

    Add more Wikipedia colors

--- apps/yapms/src/lib/components/modals/candidatemodal/presetcolors/PresetColorsModal.svelte
+++ apps/yapms/src/lib/components/modals/candidatemodal/presetcolors/PresetColorsModal.svelte
@@ -81,41 +81,48 @@
 				/>
 			{:else if tab === 1}
 				<ColorButton
-					name="Republican 1"
-					colors={[
-						'#a80000',
-						'#c21b18',
-						'#d72f30',
-						'#d75d5d',
-						'#e27f7f',
-						'#ffb2b2',
-						'#ffc8cd',
-						'#ffe0ea'
-					]}
-					onSelect={confirm}
-				/>
-				<ColorButton
-					name="Republican 2"
+					name="Republican Presidential"
 					colors={['#800000', '#aa0000', '#d40000', '#cc2f4a', '#e27f90', '#f2b3be', '#ffccd0']}
 					onSelect={confirm}
 				/>
 				<ColorButton
-					name="Democrat 1"
-					colors={[
-						'#0d0596',
-						'#3933e5',
-						'#584cde',
-						'#6674de',
-						'#7996e2',
-						'#a5b0ff',
-						'#bdd3ff',
-						'#dfeeff'
-					]}
+					name="Republican Downballot"
+					colors={['#a80000', '#c21b18', '#d72f30', '#d75d5d', '#e27f7f', '#ffb2b2', '#ffc8cd', '#ffe0ea']}
 					onSelect={confirm}
 				/>
 				<ColorButton
-					name="Democrat 2"
+					name="Democrat Presidential"
 					colors={['#002b84', '#234a99', '#466aad', '#6a89c2', '#8da8d6', '#b0c8eb', '#d3e7ff']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Democrat Downballot"
+					colors={['#0d0596', '#3933e5', '#584cde', '#6674de', '#7996e2', '#a5b0ff', '#bdd3ff', '#dfeeff']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Libertarian"
+					colors={['#ce9b1e', '#deb02a', '#f1c92a', '#ffdd55', '#ffeeaa']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Independent"
+					colors={['#555555', '#737373', '#969696', '#bdbdbd', '#d9d9d9', '#e6e6e6', '#ededed', '#f5f5f5', '#fafafa']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Write-in"
+					colors={['#165016', '#217821', '#2ca02c', '#37c837', '#5fd35f', '#87de87', '#aae5aa', '#c0f0c0', '#d2f7d2', '#e5ffe5']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Whig"
+					colors={['#8c2d04', '#cc4c02', '#ec7014', '#fe9929', '#fed463', '#fee391', '#fef4b4']}
+					onSelect={confirm}
+				/>
+				<ColorButton
+					name="Level of support"
+					colors={['#2b2457', '#28497c', '#47729e', '#7d9cbb', '#b6c8d9', '#ebeeed', '#dedebd', '#bcbc83', '#8b8b54', '#5d5d2d', '#32320c']}
 					onSelect={confirm}
 				/>
 			{:else if tab === 2}
