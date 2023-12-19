0d05adaa5fb88b9b5fb8902b2b81cd56da264bdb
Jacob Enders <jacobenders1@gmail.com>
2023-12-19 17:25:54 -0500

    Make Candidate Buttons Bigger (#536)

--- apps/yapms/src/lib/components/candidatebox/CandidateBox.svelte
+++ apps/yapms/src/lib/components/candidatebox/CandidateBox.svelte
@@ -4,6 +4,7 @@
 
 	import { calculateLumaHEX } from '$lib/utils/luma';
 	import type Candidate from '$lib/types/Candidate';
+	import { CandidateBoxOptions } from '$lib/stores/CandidateBoxOptions';
 
 	export let candidate: Candidate;
 	export let selectable: boolean;
@@ -19,28 +20,32 @@
 	}
 </script>
 
-<div class="btn-group p-0.5 pointer-events-auto">
-	<button
-		class="btn no-animation overflow-hidden p-0 transition-all"
-		class:btn-sm={selected}
-		class:btn-xs={!selected}
-		style:transition-duration={transitionSpeed}
-		style:background-color={backgroundColor}
-		style:color={textColor}
-		on:click={updateSelectedCandidate}
-	>
-		<div class="flex flex-col gap-0">
-			<div class="pt-1 pb-1 pl-2 pr-2">
-				{candidate.name}
-				{$CandidateCounts.get(candidate.id) ?? 0}
-			</div>
-			{#if candidate.margins.length > 1}
-				<div class="flex flex-row w-full h-6">
-					{#each candidate.margins as margin}
-						<div class="w-full" style:background-color={margin.color} />
-					{/each}
-				</div>
-			{/if}
+<button
+	class="overflow-hidden transition-all m-0.5 pointer-events-auto cursor-pointer font-bold rounded border-double"
+	class:text-xs={$CandidateBoxOptions.fontSize === 0}
+	class:text-sm={$CandidateBoxOptions.fontSize === 1}
+	class:text-md={$CandidateBoxOptions.fontSize === 2}
+	class:text-lg={$CandidateBoxOptions.fontSize === 3}
+	class:text-xl={$CandidateBoxOptions.fontSize === 4}
+	class:border-4={selected}
+	class:border-white={textColor === 'white'}
+	class:border-black={textColor !== 'white'}
+	style:transition-duration={transitionSpeed}
+	style:background-color={backgroundColor}
+	style:color={textColor}
+	on:click={updateSelectedCandidate}
+>
+	<div class="flex flex-col justify-between h-full">
+		<div class="px-2 py-1">
+			{candidate.name}
+			{$CandidateCounts.get(candidate.id) ?? 0}
 		</div>
-	</button>
-</div>
+		{#if candidate.margins.length > 1}
+			<div class="flex flex-row w-full h-2">
+				{#each candidate.margins as margin}
+					<div class="w-full" style:background-color={margin.color} />
+				{/each}
+			</div>
+		{/if}
+	</div>
+</button>
---
+++ apps/yapms/src/lib/stores/CandidateBoxOptions.ts
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+
+export const CandidateBoxOptions = writable({
+	fontSize: 1
+});
