97bbc3cb2a5d357558a330ad602b620e3f96be7b
Flizzyclone <stormry20@gmail.com>
2023-12-21 13:32:16 -0500

    Canadian Historical results

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/CANHistoricalMapCard.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: '2021',
+			route: '/app/can/commons/2021/results'
+		},
+		{
+			label: '2019',
+			route: '/app/can/commons/2019/results'
+		},
+		{
+			label: '2015',
+			route: '/app/can/commons/2015/results'
+		},
+		{
+			label: 'House of Commons 2021',
+			route: '/app/can/commons/2021/results'
+		},
+		{
+			label: 'House of Commons 2019',
+			route: '/app/can/commons/2019/results'
+		},
+		{
+			label: 'House of Commons 2015',
+			route: '/app/can/commons/2015/results'
+		},
+		{
+			label: 'House of Commons 2011',
+			route: '/app/can/commons/2011122/results'
+		},
+		{
+			label: 'House of Commons 2008',
+			route: '/app/can/commons/2008287/results'
+		},
+		{
+			label: 'House of Commons 2006',
+			route: '/app/can/commons/2006023/results'
+		},
+		{
+			label: 'House of Commons 2004',
+			route: '/app/can/commons/2004179/results'
+		},
+		{
+			label: 'House of Commons 2000',
+			route: '/app/can/commons/2000331/results'
+		},
+		{
+			label: 'House of Commons 1997',
+			route: '/app/can/commons/1997153/results'
+		}
+	];
+</script>
+
+<MapCard
+	name="Canadian Commons Results"
+	bg="can"
+	attribution="Photo by Tetyana Kovyrina"
+	attributionLink="https://www.pexels.com/photo/parliament-hill-in-ottawa-canada-15715832/"
+	{links}
+/>
--- apps/yapms/src/lib/components/mapcard/mapcards/CANMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/CANMapCard.svelte
@@ -14,30 +14,16 @@
 			route: '/app/can/commons/2021/results'
 		},
 		{
-			label: 'Provinces',
-			route: '/app/can/provinces'
-		},
-		{
-			label: 'House of Commons',
-			route: '/app/can/commons/next/blank'
-		},
-		{
 			label: 'Senate',
 			route: '/app/can/senate'
-		},
-		{
-			label: '2021 House of Commons Results',
-			route: '/app/can/commons/2021/results'
-		},
-		{
-			label: '2019 House of Commons Results',
-			route: '/app/can/commons/2019/results'
-		},
-		{
-			label: '2015 House of Commons Results',
-			route: '/app/can/commons/2015/results'
 		}
 	];
 </script>
 
-<MapCard name="Canada" bg="can" attribution="canada" {links} />
+<MapCard
+	name="Canada"
+	bg="can"
+	attribution="Photo by Tetyana Kovyrina"
+	attributionLink="https://www.pexels.com/photo/parliament-hill-in-ottawa-canada-15715832/"
+	{links}
+/>
