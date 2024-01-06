76064138da2cbb0e23fdd785e43b6be6d93c6a81
Flizzyclone <stormry20@gmail.com>
2024-01-06 12:23:37 -0500

    Portuguese maps

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/PRTMapCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Assembly 2024',
+			route: '/app/prt/assembly/2024070/blank'
+		},
+		{
+			label: 'Districts',
+			route: '/app/prt/districts/2021093/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="Portugal"
+	bg="prt"
+	attribution="Photo by Max Avans"
+	attributionLink="https://www.pexels.com/photo/white-concrete-building-under-blue-sky-5072289/"
+	{links}
+/>
