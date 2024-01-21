00d4df9b3682230de201fa0dd22a14e38c8641ee
Jacob Enders <>
2024-01-21 17:57:26 -0500

    test ads

--- apps/yapms/src/lib/components/sidebar/sections/ads/SideBarAds.svelte
+++ apps/yapms/src/lib/components/sidebar/sections/ads/SideBarAds.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	onMount(() => {
-		window.googletag.cmd.push(function () {
+	import { beforeUpdate, afterUpdate } from 'svelte';
+	afterUpdate(() => {
+		window.googletag.cmd.push(() => {
+			window.googletag
+				.defineSlot(
+					'/21838847269/yapms-sidebar',
+					[
+						[250, 250],
+						[200, 200]
+					],
+					'div-gpt-ad-1698618618626-0'
+				)
+				.addService(window.googletag.pubads());
+			window.googletag.pubads().collapseEmptyDivs();
+			window.googletag.enableServices();
+		});
+
+		window.googletag.cmd.push(() => {
 			window.googletag.display('div-gpt-ad-1698618618626-0');
+		});
+	});
+
+	beforeUpdate(() => {
+		window.googletag.cmd.push(() => {
+			window.googletag.destroySlots();
 		});
 	});
 </script>
