94652fecec8f976e9c7a4e1564ad872ab778af1e
Jacob Enders <jacobenders1@gmail.com>
2024-01-31 20:38:41 -0500

    Remember the users font size (#565)

--- apps/yapms/src/lib/stores/CandidateBoxOptions.ts
+++ apps/yapms/src/lib/stores/CandidateBoxOptions.ts
@@ -1,5 +1,19 @@
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export const CandidateBoxOptions = writable({
 	fontSize: 1
 });
+
+if (browser) {
+	let fontSize = Number(localStorage.getItem('CandidateBoxOptions.fontSize') ?? '1');
+	if (isNaN(fontSize)) {
+		fontSize = 1;
+	}
+	CandidateBoxOptions.set({
+		fontSize: fontSize
+	});
+	CandidateBoxOptions.subscribe((value) => {
+		localStorage.setItem('CandidateBoxOptions.fontSize', value.fontSize.toString());
+	});
+}
