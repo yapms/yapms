ec8566371ba25fbd9919fb2c4598e4a9a6898b64
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2024-02-01 20:17:32 -0500

    Irish maps (#566)

--- apps/yapms/src/routes/+page.svelte
+++ apps/yapms/src/routes/+page.svelte
@@ -33,6 +33,7 @@
 	import KorMapCard from '$lib/components/mapcard/mapcards/KORMapCard.svelte';
 	import CanHistoricalMapCard from '$lib/components/mapcard/mapcards/CANHistoricalMapCard.svelte';
 	import PrtMapCard from '$lib/components/mapcard/mapcards/PRTMapCard.svelte';
+	import IrlMapCard from '$lib/components/mapcard/mapcards/IRLMapCard.svelte';
 
 	export let data: PageData;
 
@@ -113,6 +114,7 @@
 				<NldMapCard />
 				<NzlMapCard />
 				<ZafMapCard />
+				<IrlMapCard />
 				<FraMapCard />
 				<KorMapCard />
 				<PrtMapCard />
