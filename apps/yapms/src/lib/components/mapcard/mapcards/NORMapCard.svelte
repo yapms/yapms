21fa65296e1bbf7fd622bfc2f3c45f1db91d2873
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2024-03-16 21:47:23 -0400

    Norwegian Maps (#609)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/NORMapCard.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Storting',
+			route: '/app/nor/storting/2021256/blank'
+		},
+		{
+			label: '2021 Results',
+			route: '/app/nor/storting/2021256/results'
+		},
+		{
+			label: 'Regions',
+			route: '/app/nor/regions/2024001/blank'
+		},
+		{
+			label: 'Storting 2021',
+			route: '/app/nor/storting/2021256/blank'
+		},
+		{
+			label: 'Storting 2021 Results',
+			route: '/app/nor/storting/2021256/results'
+		},
+		{
+			label: 'Regions 2024',
+			route: '/app/nor/regions/2024001/blank'
+		},
+		{
+			label: 'Storting 2017',
+			route: '/app/nor/storting/2017254/results'
+		},
+		{
+			label: 'Regions 2020',
+			route: '/app/nor/regions/2020001/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="Norway"
+	bg="nor"
+	attribution="Photo by gcardinal"
+	attributionLink="https://commons.wikimedia.org/wiki/File:Stortinget,_Oslo,_Norway.jpg"
+	{links}
+/>
