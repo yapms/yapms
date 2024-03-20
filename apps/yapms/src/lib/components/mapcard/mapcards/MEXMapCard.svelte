3c27d20ff188d445dd29ca905485d9bb5bb6c8c4
han rodríguez <han@gonzalpi.com>
2024-03-19 18:01:00 -0600

    add Mexico map card definition

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/MEXMapCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Chamber of Deputies 2024',
+			route: '/app/mex/deputies/2024/blank'
+		},
+		{
+			label: 'Senate 2024',
+			route: '/app/mex/senate/2024/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="Mexico"
+	bg="mex"
+	attribution="Photo by Thelmadatter on Wikimedia"
+	attributionLink="https://commons.wikimedia.org/wiki/File:LegPalaceDF.JPG"
+	{links}
+/>
