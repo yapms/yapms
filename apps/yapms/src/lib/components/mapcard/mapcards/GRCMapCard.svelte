58d6debb112d2c8ae04eb0f760c9ccc10a1cb495
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2024-03-22 20:59:38 -0400

    Greek maps (#613)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/GRCMapCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Parliament',
+			route: '/app/grc/parliament/2023001/blank'
+		},
+		{
+			label: 'Results June 2023',
+			route: '/app/grc/parliament/2023176/results'
+		},
+		{
+			label: 'Results May 2023',
+			route: '/app/grc/parliament/2023142/results'
+		},
+		{
+			label: 'Regions',
+			route: '/app/grc/regions/2011001/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="Greece"
+	bg="grc"
+	attribution="Photo by NikosLikomitros"
+	attributionLink="https://commons.wikimedia.org/wiki/File:Hellenic_Parliament,_general_view.jpg"
+	{links}
+/>
