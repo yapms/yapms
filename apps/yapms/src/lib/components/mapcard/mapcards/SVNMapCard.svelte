b88f37641f45bd28cb1481503ca770f81fd04a98
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2024-04-21 16:27:42 -0400

    Slovenian maps (#631)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/SVNMapCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'National Assembly',
+			route: '/app/svn/assembly/2022114/blank'
+		},
+		{
+			label: 'Results 2022',
+			route: '/app/svn/assembly/2022114/results'
+		},
+		{
+			label: 'Administrative Units',
+			route: '/app/svn/administrative_units/2024001/blank'
+		},
+		{
+			label: 'Municipalities',
+			route: '/app/svn/municipalities/2024001/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="Slovenia"
+	bg="svn"
+	attribution="Photo by Philippe Hässig"
+	attributionLink="https://en.wikipedia.org/wiki/File:Zgradba_Dr%C5%BEavnega_zbora.jpg"
+	{links}
+/>
