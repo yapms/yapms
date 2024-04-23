62caf77dfe05764e626207d593aaf56180005456
Flizzyclone <stormry20@gmail.com>
2024-04-23 13:19:22 -0400

    Indian states/Lok Sabha

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/INDMapCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Lok Sabha',
+			route: '/app/ind/lok_sabha/2024109/blank'
+		},
+		{
+			label: 'States',
+			route: '/app/ind/states/2024001/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="India"
+	bg="ind"
+	attribution="Photo by Indian Ministry of Parliamentary Affairs"
+	attributionLink="https://commons.wikimedia.org/wiki/File:Glimpse_of_the_new_Parliament_Building,_in_New_Delhi.jpg"
+	{links}
+/>
