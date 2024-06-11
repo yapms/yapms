136d211eca27da3438a9e5946902de70cc9be55d
Jacob Enders <jacobenders1@gmail.com>
2024-06-11 13:27:18 -0400

    Usa & Canada Map (#682)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/USACANMapCard.svelte
@@ -0,0 +1,18 @@
+<script>
+	import MapCard from '../MapCard.svelte';
+
+	const links = [
+		{
+			label: 'States',
+			route: '/app/usa_can/states/2024001/blank'
+		}
+	];
+</script>
+
+<MapCard
+	name="United States & Canada"
+	bg="usacan"
+	attribution="US Embassy & Consulates in Canada"
+	{links}
+	attributionLink="https://ca.usembassy.gov/united-states-canada-strength-partnership/"
+/>
