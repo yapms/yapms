e5ae2af859eb99c5b1f3e591d9d5b882c2d4b331
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2024-06-22 14:43:06 -0400

    French National Assembly map (#691)

--- apps/yapms/src/lib/components/mapcard/mapcards/FRAMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/FRAMapCard.svelte
@@ -2,6 +2,10 @@
 	import MapCard from '../MapCard.svelte';
 	const links = [
 		{
+			label: 'National Assembly',
+			route: '/app/fra/assembly/2024181/blank'
+		},
+		{
 			label: 'Departments',
 			route: '/app/fra/departments/2022/blank'
 		}
