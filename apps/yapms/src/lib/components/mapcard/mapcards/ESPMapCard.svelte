aa41d53458201663dcc947a38224cdef7fb5ec69
Jacob Enders <jacobenders1@gmail.com>
2024-10-13 11:26:51 -0400

    Add Spain (#927)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/ESPMapCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Congress of Deputies',
+			route: '/app/esp/deputies/2024/blank'
+		}
+	];
+</script>
+
+<MapCard name="Spain" bg="esp" attribution="Photo by Wolfgang Pehlemann on Wikimedia" {links} />
