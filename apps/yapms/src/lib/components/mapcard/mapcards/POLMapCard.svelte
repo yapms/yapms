264f0864d97717c7713d8b4a46c31841331dad08
Flizzyclone <stormry20@gmail.com>
2025-02-14 16:43:36 -0500

    Polish Maps

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/POLMapCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Voivodeships',
+			route: '/app/pol/regions/2025045/blank'
+		},
+		{
+			label: 'Powiats',
+			route: '/app/pol/powiats/2025045/blank'
+		},
+		{
+			label: 'Sejm',
+			route: '/app/pol/sejm/2023288/blank'
+		},
+		{
+			label: 'Sejm 2023',
+			route: '/app/pol/sejm/2023288/results'
+		}
+	];
+</script>
+
+<MapCard
+	name="Poland"
+	bg="pol"
+	attribution="Photo by Piotr VaGla Waglowski"
+	{links}
+	attributionLink="https://commons.wikimedia.org/wiki/File:20070131_sejm_widok_od_ul_matejki.jpg"
+/>
