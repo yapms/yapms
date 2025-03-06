61c34239cef4167f3a493e51865a1c9412ab7163
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2025-03-05 19:51:02 -0500

    Polish Senate (#1160)

--- apps/yapms/src/lib/components/mapcard/mapcards/POLMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/POLMapCard.svelte
@@ -14,8 +14,28 @@
 			route: '/app/pol/sejm/2023288/blank'
 		},
 		{
-			label: 'Sejm 2023',
+			label: 'Voivodeships',
+			route: '/app/pol/regions/2025045/blank'
+		},
+		{
+			label: 'Powiats',
+			route: '/app/pol/powiats/2025045/blank'
+		},
+		{
+			label: 'Sejm',
+			route: '/app/pol/sejm/2023288/blank'
+		},
+		{
+			label: 'Sejm 2023 Results',
 			route: '/app/pol/sejm/2023288/results'
+		},
+		{
+			label: 'Senate',
+			route: '/app/pol/senate/2023288/blank'
+		},
+		{
+			label: 'Senate 2023 Results',
+			route: '/app/pol/senate/2023288/results'
 		}
 	];
 </script>
