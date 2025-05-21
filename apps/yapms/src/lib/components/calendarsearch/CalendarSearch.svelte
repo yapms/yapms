7f502905375820767e59aac46bfefe6c51200d21
Jacob Enders <jacobenders1@gmail.com>
2025-05-20 23:42:14 -0400

    init

---
+++ apps/yapms/src/lib/components/calendarsearch/CalendarSearch.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { selectedCalendar = $bindable<string>('usa') } = $props();
+</script>
+
+<select class="select" bind:value={selectedCalendar}>
+	<option value="usa">United States</option>
+	<option value="can">Canada</option>
+</select>
