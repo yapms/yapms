d48a958a8dc6e8878a668e54332de52004c3fd53
Jacob Enders <jacobenders1@gmail.com>
2025-05-29 22:43:31 -0400

    work

--- apps/yapms/src/lib/components/calendar/USAPresidentialCalendar.svelte
+++ apps/yapms/src/lib/components/calendar/USAPresidentialCalendar.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import cal from '../../assets/calendar/usa.json';
-	import { parse } from 'date-fns';
+	import { format, parse } from 'date-fns';
 
 	const presidential = cal.elections.find((type) => type.type === 'presidential');
 	const presidentialElections =
 		presidential?.data
 			.sort((a, b) => (a.date < b.date ? 1 : -1))
 			.map((election) => {
-				const parsedDate = parse(
-					election.date.toString(),
-					'yyyyMMdd',
-					new Date()
-				).toLocaleDateString();
+				const parsedDate = parse(election.date.toString(), 'yyyyMMdd', new Date());
 
 				return {
-					date: parsedDate,
+					date: format(parsedDate, 'MMMM do y'),
 					mapLink: new URL(election.map_link, window.location.href),
 					candidates: election.candidates
+						// sort candidates by party name
+						.sort((a, b) => a.party.localeCompare(b.party))
+						// collect the data
+						.map((candidate) => {
+							return {
+								name: candidate.name,
+								party: candidate.party,
+								votes: candidate.votes
+							};
+						})
 				};
 			}) ?? [];
 </script>
@@ -33,16 +39,16 @@
 			{#each presidentialElections as election}
 				<tr>
 					<th>{election.date}</th>
-					<td>
-						<ul class="flex flex-col gap-2">
-							{#each election.candidates as candidate}
-								<li class="flex flex-col">
-									<span><b>{candidate.party}</b> - {candidate.name}</span>
-									<span>Votes: {candidate.votes}</span>
-								</li>
-							{/each}
-						</ul>
-					</td>
+					{#each election.candidates as candidate}
+						<td>
+							<div class="flex flex-col">
+								<span><b>{candidate.party}:</b> {candidate.name}</span>
+								{#if candidate.votes !== null}
+									<span><b>Votes:</b> {candidate.votes}</span>
+								{/if}
+							</div>
+						</td>
+					{/each}
 					<td>
 						<a href={election.mapLink.href} class="link">Open Map</a>
 					</td>
