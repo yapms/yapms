71229d15365759f55db2109c4e50a638d23b4742
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2025-06-06 23:04:45 -0400

    German Updates (#1426)

--- apps/yapms/src/lib/components/mapcard/mapcards/DEUMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/DEUMapCard.svelte
@@ -3,10 +3,30 @@
 	const links = [
 		{
 			label: 'Bundestag',
+			route: '/app/deu/bundestag/2025055/blank'
+		},
+		{
+			label: 'Results 2025',
+			route: '/app/deu/bundestag/2025055/results'
+		},
+		{
+			label: 'Bundesrat',
+			route: '/app/deu/bundesrat/2023001/blank'
+		},
+		{
+			label: 'Bundestag 2025',
+			route: '/app/deu/bundestag/2025055/blank'
+		},
+		{
+			label: 'Bundestag Results 2025',
+			route: '/app/deu/bundestag/2025055/results'
+		},
+		{
+			label: 'Bundestag 2021',
 			route: '/app/deu/bundestag/2021269/blank'
 		},
 		{
-			label: 'Results 2021',
+			label: 'Bundestag Results 2021',
 			route: '/app/deu/bundestag/2021269/results'
 		},
 		{
