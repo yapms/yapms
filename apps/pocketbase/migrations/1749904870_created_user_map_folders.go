8ae60ddf3a27c53616c075426f5f3959714407b5
Flizzyclone <stormry20@gmail.com>
2025-06-17 20:00:03 -0400

    Folders for Saved Maps

---
+++ apps/pocketbase/migrations/1749904870_created_user_map_folders.go
@@ -0,0 +1,102 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": "@request.auth.id != \"\" && @request.auth.id != \"\"",
+			"deleteRule": "@request.auth.id != \"\" && @request.auth.id ?= user.id",
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"cascadeDelete": false,
+					"collectionId": "_pb_users_auth_",
+					"hidden": false,
+					"id": "relation2375276105",
+					"maxSelect": 1,
+					"minSelect": 0,
+					"name": "user",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "relation"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1579384326",
+					"max": 0,
+					"min": 0,
+					"name": "name",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_1718115395",
+			"indexes": [],
+			"listRule": "@request.auth.id != \"\" && user.id ?= @request.auth.id",
+			"name": "user_map_folders",
+			"system": false,
+			"type": "base",
+			"updateRule": "@request.auth.id != \"\" && @request.auth.id ?= user.id",
+			"viewRule": "@request.auth.id != \"\" && user.id ?= @request.auth.id"
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1718115395")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
---
+++ apps/yapms/src/lib/components/sidebar/sections/savedmaps/SavedMapFolder.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+	import MinusCircle from '$lib/icons/MinusCircle.svelte';
+	import { PocketBaseStore } from '$lib/stores/PocketBase';
+	import { updateSavedMapFolder } from '$lib/utils/savedMaps';
+	import { useSortable } from '$lib/utils/sortableHook.svelte';
+	import SavedMap from './SavedMap.svelte';
+
+	let {
+		folderName,
+		folderID,
+		open,
+		onUpdated,
+		onToggle
+	}: {
+		folderName: string;
+		folderID: string;
+		open: boolean;
+		onUpdated: () => void;
+		onToggle: (folderID: string) => void;
+	} = $props();
+
+	const maps = $PocketBaseStore.collection('user_maps').getFullList({
+		filter: `folder = '${folderID}'`,
+		requestKey: null
+	});
+
+	let submitting = $state(false);
+
+	async function deleteFolder() {
+		submitting = true;
+		await $PocketBaseStore.collection('user_map_folders').delete(folderID);
+		submitting = false;
+		onUpdated();
+	}
+
+	let mapList = $state<HTMLDivElement | undefined>(undefined);
+
+	$effect(() => {
+		useSortable(() => mapList, {
+			animation: 140,
+			dragoverBubble: true,
+			delay: 250,
+			delayOnTouchOnly: true,
+			group: 'user-maps',
+			sort: false,
+			disabled: !open,
+			ghostClass: 'z-10',
+			async onAdd(evt) {
+				await updateSavedMapFolder(evt, folderID);
+				onUpdated();
+			}
+		});
+	});
+
+	function onCheck() {
+		onToggle(folderID);
+	}
+</script>
+
+<div class="collapse bg-base-300 border-base-300 rounded-r-none">
+	<input type="checkbox" bind:checked={open} onchange={onCheck} />
+	<div class="collapse-title join p-0 flex items-center transition-[padding]" class:pb-2={open}>
+		<p class="text-xs font-semibold p-0 grow text-center">{folderName}</p>
+		<div class="tooltip tooltip-left z-10" data-tip="Delete">
+			<button
+				class="btn btn-sm btn-error flex-shrink join-item"
+				class:rounded-br-none={open}
+				class:rounded-bl-sm={open}
+				onclick={deleteFolder}
+				disabled={submitting}
+			>
+				<MinusCircle class="w-6 h-6" />
+			</button>
+		</div>
+	</div>
+
+	<div class="collapse-content">
+		<div class="flex flex-col gap-2 min-h-8" bind:this={mapList}>
+			{#await maps}
+				<div class="flex justify-center">
+					<span class="loading loading-dots loading-lg"></span>
+				</div>
+			{:then maps}
+				<div
+					class="absolute w-[calc(100%-8*var(--spacing))] h-8 rounded-md border-2 border-dashed border-opacity-20 border-base-content/30 z-10"
+				></div>
+				{#each maps.sort((a, b) => a.name.localeCompare(b.name)) as map}
+					<SavedMap mapName={map.name} mapID={map.id} {onUpdated} />
+				{/each}
+			{/await}
+		</div>
+	</div>
+</div>
