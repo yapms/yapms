48ab57cf5adf7b669f57009b12df65e15860df23
Jacob Enders <jacobenders1@gmail.com>
2025-06-22 14:13:38 -0400

    draw save file even if not navigating

--- apps/yapms/src/lib/components/modals/sharemodal/ShareModal.svelte
+++ apps/yapms/src/lib/components/modals/sharemodal/ShareModal.svelte
@@ -4,7 +4,7 @@
 	import { ShareModalStore } from '$lib/stores/Modals';
 	import { downloadJson, generateJson } from '$lib/utils/saveMap';
 	import Link from '$lib/icons/Link.svelte';
-	import { gotoLoadedMap, setLoadedMapFromFile } from '$lib/stores/LoadedMap';
+	import { drawLoadedMap, gotoLoadedMap, setLoadedMapFromFile } from '$lib/stores/LoadedMap';
 	import ExclamationCircle from '$lib/icons/ExclamationCircle.svelte';
 	import CheckCircle from '$lib/icons/CheckCircle.svelte';
 	import { get } from 'svelte/store';
@@ -38,7 +38,9 @@
 
 	function load() {
 		if (files && files.length > 0) {
-			setLoadedMapFromFile(files).then(() => gotoLoadedMap({ s: true }));
+			setLoadedMapFromFile(files)
+				.then(() => gotoLoadedMap({ s: true }))
+				.then(drawLoadedMap);
 			ShareModalStore.set({ ...$ShareModalStore, open: false });
 		}
 	}
