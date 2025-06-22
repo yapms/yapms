f14c1338b8d0c3fc3497919e4ea463d13654c237
Jacob Enders <jacobenders1@gmail.com>
2025-06-22 11:06:06 -0400

    init

---
+++ apps/yapms/src/lib/components/modals/toolsmodal/tools/SimulatorModal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { SimulatorModalStore } from '$lib/stores/Modals';
+	import ModalBase from '../../ModalBase.svelte';
+	import { RegionsStore } from '$lib/stores/regions/Regions';
+	import { CandidatesStore } from '$lib/stores/Candidates';
+
+	function calculate() {}
+</script>
+
+<ModalBase title="Simulator" store={SimulatorModalStore}>
+	<div slot="content">
+		<button class="btn" onclick={calculate}>Start</button>
+		<fieldset class="fieldset">
+			{#each $RegionsStore.sort() as region}
+				<table class="table">
+					<thead>
+						<tr>
+							<td>{region.longName}</td>
+						</tr>
+					</thead>
+					<tbody>
+						{#each $CandidatesStore as candidate}
+							<tr>
+								<td class="w-[50%]">{candidate.name}</td>
+								<td><input type="range" step="1" min="0" max="100" class="range" /></td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			{/each}
+		</fieldset>
+	</div>
+</ModalBase>
