2da8017ca7ef1141e9b8922eacfafbac20100a3d
Jacob Enders <jacobenders1@gmail.com>
2025-06-28 15:20:08 -0400

    init

--- apps/yapms/src/lib/stores/LoadedMap.ts
+++ apps/yapms/src/lib/stores/LoadedMap.ts
@@ -41,29 +41,34 @@ export async function setLoadedMapFromTCTFile(files: FileList) {
 	return new Promise<undefined>((resolve, reject) => {
 		const fileReader = new FileReader();
 
-		fileReader.onload = function () {
+		fileReader.onload = function() {
 			if (typeof fileReader.result !== 'string') {
 				return reject('Failed to read file.');
 			}
-			const fileData = fileReader.result.toString();
-			const reverseFileData = fileData.split('').reverse().join('');
-			const decodedFileData = safeATOB(reverseFileData);
-			if (decodedFileData.isErr()) {
-				return reject('Failed to read file.');
+
+			const rawData = fileReader.result.toString();
+			const isJSON = safeJsonParse(rawData).isOk();
+			const data = isJSON ? ok(rawData) : decodeTCTData(rawData);
+
+			if (data.isErr()) {
+				return reject(data.error);
 			}
-			const jsonData = safeJsonParse(decodedFileData.value);
-			if (jsonData.isErr()) {
-				return reject('Failed to read file.');
+
+			const parsedData = safeJsonParse(data.value);
+			if (parsedData.isErr()) {
+				return reject("Failed to read file.");
 			}
-			const yapmsData = convertTCTJsontoYapmsJson(jsonData.value);
+
+			const yapmsData = convertTCTJsontoYapmsJson(parsedData.value);
 			if (yapmsData.isErr()) {
 				return reject(yapmsData.error);
 			}
+
 			setLoadedMapFromJson(yapmsData.value);
-			resolve(undefined);
+			return resolve(undefined);
 		};
 
-		fileReader.onerror = function () {
+		fileReader.onerror = function() {
 			console.error(fileReader.error);
 			reject(undefined);
 		};
@@ -72,11 +77,17 @@ export async function setLoadedMapFromTCTFile(files: FileList) {
 	});
 }
 
+function decodeTCTData(data: string) {
+	const reversedData = data.split('').reverse().join('');
+	const decodedData = safeATOB(reversedData);
+	return decodedData;
+}
+
 export async function setLoadedMapFromFile(files: FileList) {
 	return new Promise((resolve, reject) => {
 		const fileReader = new FileReader();
 
-		fileReader.onload = async function () {
+		fileReader.onload = async function() {
 			if (typeof fileReader.result !== 'string') {
 				return;
 			}
@@ -85,7 +96,7 @@ export async function setLoadedMapFromFile(files: FileList) {
 			resolve(undefined);
 		};
 
-		fileReader.onerror = function () {
+		fileReader.onerror = function() {
 			console.error(fileReader.error);
 			reject(undefined);
 		};
@@ -110,10 +121,10 @@ export function setLoadedMapFromJson(data: unknown) {
 export async function gotoLoadedMap(
 	options:
 		| {
-				m?: string | undefined;
-				um?: string | undefined;
-				s?: boolean | undefined;
-		  }
+			m?: string | undefined;
+			um?: string | undefined;
+			s?: boolean | undefined;
+		}
 		| undefined = undefined
 ) {
 	const data = get(LoadedMapStore);
@@ -186,17 +197,30 @@ export async function drawLoadedMap() {
 }
 
 function convertTCTJsontoYapmsJson(tct: unknown) {
-	const parsedData = FileSchema_TCT.safeParse(tct);
-	if (parsedData.success === false) {
-		return err('File data does not match TCT format.');
+	const nct = FileSchema_NCT.safeParse(tct);
+	const showcase = FileSchema_ShowCase.safeParse(tct);
+
+	if (nct.success === false && showcase.success === false) {
+		return err('File data does not match a supported format.');
+	}
+
+	const country = 'usa';
+	const type = 'presidential';
+	const year = '2028';
+	let variant = 'takeall';
+
+	if (nct.success) {
+		variant = getVariantFromNCT(nct.data);
+	} else if (showcase.success) {
+		variant = getVariantFromShowcase(showcase.data);
 	}
 
 	const yapmsData = {
 		map: {
-			country: 'usa',
-			type: 'presidential',
-			year: '2028',
-			variant: 'takeall'
+			country: country,
+			type: type,
+			year: year,
+			variant: variant
 		},
 		tossup: {
 			id: '',
@@ -225,49 +249,15 @@ function convertTCTJsontoYapmsJson(tct: unknown) {
 		}>()
 	};
 
-	// Add all the candidates to the YAPms candidates list.
-	for (const candidate of parsedData.data.overall_results) {
-		yapmsData.candidates.push({
-			id: candidate.candidate.toString(),
-			name: candidate.candidate_name ?? candidate.candidate.toString(),
-			defaultCount: 0,
-			margins: [
-				{
-					color:
-						candidate.candidate_color ??
-						'#' +
-							Math.trunc(Math.random() * 16777215)
-								.toString(16)
-								.padStart(6, '0')
-				}
-			]
-		});
-	}
-
-	// Add all the state results to the YAPms regions.
-	for (const state of parsedData.data.state_results) {
-		const value = state.result.reduce((prev, curr) => {
-			return prev + curr.electoral_votes;
-		}, 0);
-
-		const candidates = state.result.map((candidate) => {
-			return {
-				id: candidate.candidate.toString(),
-				count: candidate.electoral_votes,
-				margin: 0
-			};
-		});
-
-		const abbr = ['NE', 'ME'].includes(state.abbr) ? state.abbr : state.abbr.toLowerCase();
-		yapmsData.regions.push({
-			id: abbr,
-			value: value,
-			permaVal: value,
-			locked: false,
-			permaLocked: false,
-			disabled: false,
-			candidates: candidates
-		});
+	if (nct.success) {
+		alert("NCT");
+		yapmsData.candidates = getCandidatesFromNCT(nct.data);
+		yapmsData.regions = getRegionsFromNCT(nct.data);
+	} else if (showcase.success) {
+		alert("Showcase");
+		yapmsData.candidates = getCandidatesFromShowcase(showcase.data);
+		console.log(yapmsData.candidates);
+		yapmsData.regions = getRegionsFromShowCase(showcase.data);
 	}
 
 	for (const region of yapmsData.regions) {
@@ -277,7 +267,7 @@ function convertTCTJsontoYapmsJson(tct: unknown) {
 			}) !== -1;
 
 		if (found === false) {
-			return err('This TCT map is not supported.');
+			return err(`This TCT map is not supported. Region: ${region.id}`);
 		}
 	}
 
@@ -304,7 +294,202 @@ function convertTCTJsontoYapmsJson(tct: unknown) {
 	return ok(yapmsData);
 }
 
-const FileSchema_TCT = z.object({
+function getVariantFromNCT(data: z.infer<typeof FileSchema_NCT>) {
+	for (const state of data.state_results) {
+		console.log(state.abbr);
+		if (['m1', 'm2', 'n1', 'n2', 'n3'].includes(state.abbr.toLowerCase())) {
+			return 'blank';
+		}
+	}
+	return 'takeall';
+}
+
+function getCandidatesFromNCT(data: z.infer<typeof FileSchema_NCT>) {
+	const candidates = new Map<string, { color: string; name: string; }>();
+	for (const candidate of data.overall_results) {
+		candidates.set(candidate.candidate.toString(), {
+			name: candidate.candidate_name ?? '',
+			color: candidate.candidate_color ?? '#' + Math.trunc(Math.random() * 16777215).toString(16).padStart(6, '0')
+		});
+	}
+
+	return candidates.entries().map((candidate) => {
+		const key = candidate[0];
+		const value = candidate[1];
+		return {
+			id: key,
+			name: value.name,
+			defaultCount: 0,
+			margins: [{
+				color: value.color
+			}],
+		}
+	}).toArray();
+}
+
+function getRegionsFromNCT(data: z.infer<typeof FileSchema_NCT>) {
+	const regions = [];
+	const variant = getVariantFromNCT(data);
+	for (const state of data.state_results) {
+		const value = state.result.reduce((prev, curr) => {
+			return prev + curr.electoral_votes;
+		}, 0);
+
+		const candidates = state.result.map((candidate) => {
+			return {
+				id: candidate.candidate.toString(),
+				count: candidate.electoral_votes,
+				margin: 0
+			};
+		});
+
+		let abbr = state.abbr.toLowerCase();
+		if (variant === 'blank') {
+			switch (abbr) {
+				case 'me':
+					abbr = 'me-al';
+					break;
+				case 'm1':
+					abbr = 'me-01';
+					break;
+				case 'm2':
+					abbr = 'me-02';
+					break;
+				case 'ne':
+					abbr = 'ne-al';
+					break;
+				case 'n1':
+					abbr = 'ne-01';
+					break;
+				case 'n2':
+					abbr = 'ne-02';
+					break;
+				case 'n3':
+					abbr = 'ne-03';
+					break;
+			}
+		} else if (variant === 'takeall') {
+			switch (abbr) {
+				case 'me':
+					abbr = 'ME';
+					break;
+				case 'ne':
+					abbr = 'NE';
+					break;
+			}
+		}
+		regions.push({
+			id: abbr,
+			value: value,
+			permaVal: value,
+			locked: false,
+			permaLocked: false,
+			disabled: false,
+			candidates: candidates
+		});
+	}
+	return regions;
+}
+
+function getVariantFromShowcase(data: z.infer<typeof FileSchema_ShowCase>) {
+	for (const state of data.results) {
+		if (['m1', 'm2', 'n1', 'n2', 'n3'].includes(state.state.toLowerCase())) {
+			return 'blank';
+		}
+	}
+	return 'takeall';
+}
+
+function getCandidatesFromShowcase(data: z.infer<typeof FileSchema_ShowCase>) {
+	const candidates = new Map<string, { color: string; name: string; }>();
+	for (const state of data.results) {
+		for (const result of state.results) {
+			candidates.set(result.candidate_name, {
+				name: result.candidate_name,
+				color: '#' + Math.trunc(Math.random() * 16777215).toString(16).padStart(6, '0')
+			});
+		}
+	}
+
+	return candidates.entries().map((candidate) => {
+		const key = candidate[0];
+		const value = candidate[1];
+		return {
+			id: key,
+			name: value.name,
+			defaultCount: 0,
+			margins: [{
+				color: value.color
+			}],
+		}
+	}).toArray();
+}
+
+function getRegionsFromShowCase(data: z.infer<typeof FileSchema_ShowCase>) {
+	const regions = [];
+	const variant = getVariantFromShowcase(data);
+	for (const result of data.results) {
+		const value = result.results.reduce((prev, curr) => {
+			return prev + curr.electoral_votes;
+		}, 0);
+
+		const candidates = result.results.map((candidate) => {
+			return {
+				id: candidate.candidate_name,
+				count: candidate.electoral_votes,
+				margin: 0
+			};
+		});
+
+		let abbr = result.state.toLowerCase();
+		if (variant === 'blank') {
+			switch (abbr) {
+				case 'me':
+					abbr = 'me-al';
+					break;
+				case 'm1':
+					abbr = 'me-01';
+					break;
+				case 'm2':
+					abbr = 'me-02';
+					break;
+				case 'ne':
+					abbr = 'ne-al';
+					break;
+				case 'n1':
+					abbr = 'ne-01';
+					break;
+				case 'n2':
+					abbr = 'ne-02';
+					break;
+				case 'n3':
+					abbr = 'ne-03';
+					break;
+			}
+		} else if (variant === 'takeall') {
+			switch (abbr) {
+				case 'me':
+					abbr = 'ME';
+					break;
+				case 'ne':
+					abbr = 'NE';
+					break;
+			}
+		}
+		regions.push({
+			id: abbr,
+			value: value,
+			permaVal: value,
+			locked: false,
+			permaLocked: false,
+			disabled: false,
+			candidates: candidates
+		});
+	}
+	return regions;
+}
+
+const FileSchema_NCT = z.object({
 	overall_results: z.array(
 		z.object({
 			candidate_name: z.string().optional(),
@@ -330,4 +515,16 @@ const FileSchema_TCT = z.object({
 			result_time: z.number()
 		})
 	)
+});
+
+const FileSchema_ShowCase = z.object({
+	results: z.array(z.object({
+		state: z.string(),
+		results: z.array(z.object({
+			candidate_name: z.string(),
+			electoral_votes: z.number(),
+			popular_votes: z.number(),
+			vote_percentage: z.number(),
+		}))
+	}))
 });
