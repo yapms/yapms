cc1078d12f67c1b77ddc36057cd0be8db327760c
Flizzyclone <stormry20@gmail.com>
2025-06-28 13:45:42 -0400

    Properly handle create failure

--- apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
+++ apps/yapms/src/lib/components/sidebar/sections/savedmaps/SaveMap.svelte
@@ -4,12 +4,13 @@
 	import PlusCircle from '$lib/icons/PlusCircle.svelte';
 	import { page } from '$app/stores';
 	import Folder from '$lib/icons/Folder.svelte';
+	import ExclamationCircle from '$lib/icons/ExclamationCircle.svelte';
 
-	export let disabled = false;
-	export let onSubmitted: () => void;
+	let { disabled, onSubmitted }: { disabled: boolean, onSubmitted: () => void } = $props();
 
-	let submitting = false;
-	let newMapName = '';
+	let error = $state<string>('');
+	let submitting = $state<boolean>(false);
+	let newMapName = $state<string>('');
 
 	async function createMap() {
 		if (newMapName === '') {
@@ -19,6 +20,7 @@
 			return;
 		}
 		submitting = true;
+		error = '';
 		const form = new FormData();
 		const newMapData = new File([JSON.stringify(generateJson())], 'data.json', {
 			type: 'application/json'
@@ -26,12 +28,18 @@
 		form.append('data', newMapData);
 		form.append('name', newMapName);
 		form.append('user', $PocketBaseStore.authStore.record.id);
-		await $PocketBaseStore.collection('user_maps').create(form);
-		submitting = false;
-		newMapName = '';
-		onSubmitted();
+		await $PocketBaseStore.collection('user_maps').create(form)
+		.then(() => {
+			submitting = false;
+			newMapName = '';
+			onSubmitted();
+		})
+		.catch((e) => {
+			console.error(e);
+			error = e.message;
+			submitting = false;
+		});
 	}
-	console.log($page.url.pathname.split('/').at(2));
 
 	async function createFolder() {
 		if (newMapName === '') {
@@ -41,15 +49,29 @@
 			return;
 		}
 		submitting = true;
+		error = '';
 		await $PocketBaseStore.collection('user_map_folders').create({
 			name: newMapName,
 			user: $PocketBaseStore.authStore.record.id
+		}).then(() => {
+			submitting = false;
+			newMapName = '';
+			onSubmitted();
+		})
+		.catch((e) => {
+			console.error(e);
+			error = e.message;
+			submitting = false;
 		});
-		submitting = false;
-		newMapName = '';
-		onSubmitted();
 	}
 </script>
+
+{#if error}
+	<div class="alert alert-error gap-x-2 p-2">
+		<ExclamationCircle class="w-5 h-5" />
+		<span>{error}</span>
+	</div>
+{/if}
 
 <div class="join">
 	<input
@@ -62,7 +84,7 @@
 	<div class="tooltip" data-tip="New Folder">
 		<button
 			class="btn btn-sm join-item"
-			on:click={createFolder}
+			onclick={createFolder}
 			disabled={disabled || submitting || $page.url.pathname === '/app/imported'}
 		>
 			{#if submitting}
@@ -75,7 +97,7 @@
 	<div class="tooltip" data-tip="New Map">
 		<button
 			class="btn btn-sm btn-primary join-item"
-			on:click={createMap}
+			onclick={createMap}
 			disabled={disabled || submitting || $page.url.pathname === '/app/imported'}
 		>
 			{#if submitting}
