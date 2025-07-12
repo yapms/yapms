529e09fa61e1ab94543e715f18e7949de53c418a
Jacob Enders <jacobenders1@gmail.com>
2025-07-12 14:21:35 -0400

    init

--- apps/yapms/src/lib/components/modals/candidatemodal/presetcolors/PresetColorsModal.svelte
+++ apps/yapms/src/lib/components/modals/candidatemodal/presetcolors/PresetColorsModal.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import { CustomColorsStore } from '$lib/stores/CustomColors';
 	import {
-		AddCandidateModalStore,
 		PresetColorsModalStore,
-		PresetColorsModalSelectedStore,
 		AddCustomColorModalStore,
 		EditCustomColorModalStore
 	} from '$lib/stores/Modals';
-	import ModalBase from '../../ModalBase.svelte';
+	import NewModalBase from '../../NewModalBase.svelte';
 	import ColorButton from '../customcolors/ColorButton.svelte';
 	import PresetColorsHeader from './PresetColorsHeader.svelte';
 
-	let tab = 0;
+	let {
+		open = $bindable(),
+		onSelectColors
+	}: { open: boolean; onSelectColors: (margins: string[]) => void } = $props();
+
+	let tab = $state(0);
 
 	function addCustomColor() {
 		$AddCustomColorModalStore.open = true;
@@ -31,59 +34,49 @@
 	}
 
 	function close() {
-		if ($AddCustomColorModalStore.open === true || $EditCustomColorModalStore.open === true) {
-			return;
-		}
-		$PresetColorsModalStore.open = false;
-		$AddCandidateModalStore.open = true;
-	}
-
-	function confirm(margins: string[]) {
-		$PresetColorsModalStore.open = false;
-		$PresetColorsModalSelectedStore = margins;
-		$AddCandidateModalStore.open = true;
+		open = false;
 	}
 </script>
 
-<ModalBase title="Select Preset Colors" store={PresetColorsModalStore} onClose={close}>
-	<div slot="header">
+<NewModalBase title="Select Preset Colors" {open} onClose={close}>
+	{#snippet header()}
 		<div class="flex justify-center">
 			<PresetColorsHeader bind:tab />
 		</div>
-	</div>
-	<div slot="content">
+	{/snippet}
+	{#snippet content()}
 		<div class="flex flex-col gap-2 overflow-hidden">
 			{#if tab === 0}
 				<ColorButton
 					name="Red"
 					colors={['#bf1d29', '#ff5865', '#ff8b98', '#cf8980']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Blue"
 					colors={['#1c408c', '#577ccc', '#8aafff', '#949bb3']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Green"
 					colors={['#1c8c28', '#50c85e', '#8aff97', '#7a997e']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Yellow"
 					colors={['#e6b700', '#e8c84d', '#ffe78a', '#b8a252']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Purple"
 					colors={['#822194', '#ae20c6', '#db14ff', '#a369ae']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 			{:else if tab === 1}
 				<ColorButton
 					name="Republican Presidential"
 					colors={['#800000', '#aa0000', '#d40000', '#cc2f4a', '#e27f90', '#f2b3be', '#ffccd0']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Republican Downballot"
@@ -97,12 +90,12 @@
 						'#ffc8cd',
 						'#ffe0ea'
 					]}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Democrat Presidential"
 					colors={['#002b84', '#234a99', '#466aad', '#6a89c2', '#8da8d6', '#b0c8eb', '#d3e7ff']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Democrat Downballot"
@@ -116,12 +109,12 @@
 						'#bdd3ff',
 						'#dfeeff'
 					]}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Libertarian"
 					colors={['#ce9b1e', '#deb02a', '#f1c92a', '#ffdd55', '#ffeeaa']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Independent"
@@ -136,7 +129,7 @@
 						'#f5f5f5',
 						'#fafafa'
 					]}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Write-in"
@@ -152,12 +145,12 @@
 						'#d2f7d2',
 						'#e5ffe5'
 					]}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Whig"
 					colors={['#8c2d04', '#cc4c02', '#ec7014', '#fe9929', '#fed463', '#fee391', '#fef4b4']}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 				<ColorButton
 					name="Level of support"
@@ -174,26 +167,26 @@
 						'#5d5d2d',
 						'#32320c'
 					]}
-					onSelect={confirm}
+					onSelect={onSelectColors}
 				/>
 			{:else if tab === 2}
 				{#if $CustomColorsStore.length === 0}
-					<button class="btn btn-success" on:click={addCustomColor}>Add Color</button>
+					<button class="btn btn-success" onclick={addCustomColor}>Add Color</button>
 				{/if}
 				{#each $CustomColorsStore as colors, index}
 					<ColorButton
 						{colors}
-						onSelect={confirm}
+						onSelect={onSelectColors}
 						onEdit={() => editCustomColor(index)}
 						onDelete={() => removeCustomColor(index)}
 					/>
 				{/each}
 			{/if}
 		</div>
-	</div>
-	<div slot="action">
+	{/snippet}
+	{#snippet action()}
 		{#if tab === 2 && $CustomColorsStore.length !== 0}
-			<button class="btn btn-success" on:click={addCustomColor}>Add</button>
+			<button class="btn btn-success" onclick={addCustomColor}>Add</button>
 		{/if}
-	</div>
-</ModalBase>
+	{/snippet}
+</NewModalBase>
