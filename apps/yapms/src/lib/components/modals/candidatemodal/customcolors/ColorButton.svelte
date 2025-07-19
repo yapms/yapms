e61a1c7d9ca98c06fcdbd4668f2a9b415b35dd73
Jacob Enders <jacobenders1@gmail.com>
2025-07-18 23:40:09 -0400

    work

--- apps/yapms/src/lib/components/modals/candidatemodal/customcolors/ColorButton.svelte
+++ apps/yapms/src/lib/components/modals/candidatemodal/customcolors/ColorButton.svelte
@@ -2,42 +2,66 @@
 	import Cog6Tooth from '$lib/icons/Cog6Tooth.svelte';
 	import MinusCircle from '$lib/icons/MinusCircle.svelte';
 
-	export let colors: string[] = [];
-	export let name: string | undefined = undefined;
+	let {
+		name,
+		colors,
+		selected,
+		hideName = false,
+		onSelect = undefined,
+		onConfirm = undefined,
+		onEdit = undefined,
+		onDelete = undefined
+	}: {
+		name: string;
+		colors: string[];
+		selected: boolean;
+		hideName?: boolean;
+		onSelect?: ((name: string) => void) | undefined;
+		onConfirm?: ((colors: string[]) => void) | undefined;
+		onEdit?: (() => void) | undefined;
+		onDelete?: (() => void) | undefined;
+	} = $props();
 
-	export let onSelect: (colors: string[]) => void;
-	export let onEdit: (() => void) | undefined = undefined;
-	export let onDelete: (() => void) | undefined = undefined;
+	function onSelectColors() {
+		onSelect?.(name ?? '');
+	}
+
+	function onConfirmColors() {
+		onConfirm?.(colors);
+	}
 </script>
 
 <div class="join">
 	{#if onEdit !== undefined}
-		<button class="btn btn-lg btn-primary join-item" on:click={onEdit}
-			><Cog6Tooth class="w-6 h-6" /></button
-		>
+		<button class="btn btn-lg btn-primary join-item" onclick={onEdit}>
+			<Cog6Tooth class="w-6 h-6" />
+		</button>
 	{/if}
 	<button
 		class="btn btn-lg join-item flex-grow flex-shrink"
-		class:h-fit={name !== undefined}
-		on:click={() => onSelect(colors)}
+		class:h-fit={name !== undefined && hideName === false}
+		onclick={onSelectColors}
 	>
 		<div class="flex flex-col py-2 items-center gap-1">
-			{#if name !== undefined}
+			{#if name !== undefined && hideName === false}
 				<h3 class="font-semibold">{name}</h3>
 			{/if}
 			<div class="flex flex-row flex-wrap gap-2">
 				{#each colors as color}
 					<div
-						class="outline outline-1 outline-white w-4 h-4 rounded-full"
+						class="outline-1 outline-white w-4 h-4 rounded-full"
 						style:background-color={color}
 					></div>
 				{/each}
 			</div>
 		</div>
 	</button>
+	{#if selected === true}
+		<button class="btn btn-primary h-auto join-item" onclick={onConfirmColors}> Confirm </button>
+	{/if}
 	{#if onDelete !== undefined}
-		<button class="btn btn-lg btn-error join-item" on:click={onDelete}
-			><MinusCircle class="w-6 h-6" /></button
-		>
+		<button class="btn btn-lg btn-error join-item" onclick={onDelete}>
+			<MinusCircle class="w-6 h-6" />
+		</button>
 	{/if}
 </div>
