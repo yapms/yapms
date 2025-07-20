5ee8ee53329b78160dc45ce3fdcd462cbf867446
Flizzyclone <stormry20@gmail.com>
2025-07-20 16:14:59 -0400

    UK Rework + Expansion

--- apps/yapms/src/lib/components/mapcard/mapcards/GBRMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/GBRMapCard.svelte
@@ -26,8 +26,36 @@
 			route: '/app/gbr/commons/2024185/results'
 		},
 		{
-			label: '2019 House of Commons Results',
-			route: '/app/gbr/commons/2019/results'
+			label: 'Scottish Parliament 2026',
+			route: '/app/gbr/scotland/2025121/blank'
+		},
+		{
+			label: '2021 Scottish Parliament Results',
+			route: '/app/gbr/scotland/2021126/results'
+		},
+		{
+			label: 'Welsh Senedd 2026',
+			route: '/app/gbr/senedd/2025070/blank'
+		},
+		{
+			label: '2021 Welsh Senedd Results',
+			route: '/app/gbr/senedd/2021121/results'
+		},
+		{
+			label: 'Northern Irish Assembly',
+			route: '/app/gbr/ni_assembly/2025001/blank'
+		},
+		{
+			label: '2022 Northern Irish Assembly Results',
+			route: '/app/gbr/ni_assembly/2022125/results'
+		},
+		{
+			label: '2024 London Assembly Results',
+			route: '/app/gbr/ldn_assembly/2024122/results'
+		},
+		{
+			label: 'European Electoral Regions 2018',
+			route: '/app/gbr/european_regions/2018001/blank'
 		},
 		{
 			label: 'Historic Counties',
@@ -36,7 +64,11 @@
 		{
 			label: 'Counties and Unitary Authorities',
 			route: '/app/gbr/counties/2023/blank'
-		}
+		},
+		{
+			label: 'Ceremonial Counties',
+			route: '/app/gbr/ceremonial_counties/2025001/blank'
+		},
 	];
 </script>
 
