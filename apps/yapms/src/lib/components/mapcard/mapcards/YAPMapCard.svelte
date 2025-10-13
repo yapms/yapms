9ec96b601634fb51a1f77fa1ec185d8962ee5dd0
Jacob Enders <jacobenders1@gmail.com>
2025-10-12 21:20:54 -0400

    Yapmeria (#1718)

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/YAPMapCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [
+		{
+			label: 'Presidential',
+			route: '/app/yap/presidential/2025285/blank'
+		},
+		{
+			label: 'Senate',
+			route: '/app/yap/senate/2025285/blank'
+		}
+	];
+</script>
+
+<MapCard name="Yapmeria" bg="yap" attribution="" attributionLink="" {links} />
