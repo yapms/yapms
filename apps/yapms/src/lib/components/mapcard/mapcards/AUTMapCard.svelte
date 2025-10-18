7b77fb1fca9c8e6b0bf1d31e23e1c180bd01c666
Jacob Enders <jacobenders1@gmail.com>
2025-10-18 12:15:57 -0400

    init

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/AUTMapCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+	const links = [{ label: 'States', route: '/app/aut/states/2025291/blank' }];
+</script>
+
+<MapCard
+	name="Austria"
+	bg="aut"
+	attribution="Photo by Jean Fonseca"
+	attributionLink="https://commons.wikimedia.org/wiki/File:WienParlament.jpg"
+	{links}
+/>
