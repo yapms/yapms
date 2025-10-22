e5453e7fdaeb4296ab9715cbec1aeff3fe10f29a
Flizzyclone <stormry20@gmail.com>
2025-10-21 20:32:29 -0400

    YAPms Redistricting Council Fair Map

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/YRCMapCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import MapCard from '../MapCard.svelte';
+
+	const links = [
+		{
+			label: 'Fair Map',
+			route: '/app/yrc/fair/2025294/blank'
+		},
+		{
+			label: 'Fair Map (2024 Pres)',
+			route: '/app/yrc/fair/2025294/2024_pres'
+		}
+	];
+</script>
+
+<MapCard
+	name="YAPms Redistricting Council"
+	bg="usa"
+	attribution="Photo by Andy Feliciotti"
+	attributionLink="https://unsplash.com/photos/brown-concrete-building-near-green-trees-during-daytime-6kA9FjqUxhM"
+	{links}
+/>
