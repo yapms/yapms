01fe11aa18e1f2565cff0e8c166d522ed1227bfa
svetelkiagov-hub <svetelkia.gov@gmail.com>
2025-12-01 16:45:57 +0100

    Create SVTMapCard.svelte

---
+++ apps/yapms/src/lib/components/mapcard/mapcards/SVTMapCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MapCard from '../../MapCard.svelte"
+	const links = [
+		{
+			label: 'Administrative Divisions',
+			route: '/app/svt/administrative_divisions/2025001/blank'
+		}
+	];
+
+</script>
+
+<MapCard
+	name="Kingdom of Svetelkia"
+	bg="kos" 
+	attribution="Flag of Svetelkia"
+	attributionLink="https://github.com/svetelkia-hub" 
+	{links}
+/>
