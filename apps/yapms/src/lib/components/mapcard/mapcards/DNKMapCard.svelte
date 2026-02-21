21931fdf6c50043cc74eff6912d62e640011882b
Flizzyclone <stormry20@gmail.com>
2026-02-21 04:17:04 -0500

    reset map cards

--- apps/yapms/src/lib/components/mapcard/mapcards/DNKMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/DNKMapCard.svelte
@@ -1,47 +1,29 @@
 <script lang="ts">
 	import MapCard from '../MapCard.svelte';
-	const groups = [
+	const links = [
 		{
 			label: 'Folketing',
-			routes: [
-				{
-					label: 'Folketing',
-					modalLabel: '2022 Folketing',
-					route: '/app/dnk/folketing/2022/blank'
-				},
-				{
-					label: '2022 Results',
-					modalLabel: '2022 Folketing Results',
-					route: '/app/dnk/folketing/2022/results'
-				}
-			]
+			route: '/app/dnk/folketing/2022/blank'
 		},
 		{
-			label: '',
-			routes: [
-				{
-					label: 'Municipalities',
-					route: '/app/dnk/municipalities/2021/blank'
-				},
-				{
-					label: 'Regions',
-					route: '/app/dnk/regions/2021/blank'
-				}
-			]
-		}
-	];
-
-	const imageLinks = [
-		{
-			label: 'Christiansborg Palace by merkulos',
-			route: 'https://pixabay.com/photos/christiansborg-palace-copenhagen-3700331/'
+			label: '2022 Results',
+			route: '/app/dnk/folketing/2022/results'
 		},
 		{
-			label: 'Copenhagen by Efrem Efre',
-			route:
-				'https://www.pexels.com/photo/scenic-view-of-nyhavn-canal-in-copenhagen-denmark-31400921/'
+			label: 'Municipalities',
+			route: '/app/dnk/municipalities/2021/blank'
+		},
+		{
+			label: 'Regions',
+			route: '/app/dnk/regions/2021/blank'
 		}
 	];
 </script>
 
-<MapCard name="Denmark" bg="dnk" {imageLinks} {groups} />
+<MapCard
+	name="Denmark"
+	bg="dnk"
+	attribution="Photo by merkulos"
+	attributionLink="https://pixabay.com/photos/christiansborg-palace-copenhagen-3700331/"
+	{links}
+/>
--- apps/yapms/src/lib/components/mapcard/mapcards/ITAMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/ITAMapCard.svelte
@@ -1,46 +1,41 @@
 <script lang="ts">
 	import MapCard from '../MapCard.svelte';
-	const groups = [
+	const links = [
 		{
 			label: 'Chamber of Deputies',
-			routes: [
-				{
-					label: 'Chamber of Deputies',
-					route: '/app/ita/deputies/2022/blank'
-				},
-				{
-					label: '2022 Chamber Results',
-					modalLabel: '2022 Chamber of Deputies Results',
-					route: '/app/ita/deputies/2022/results'
-				}
-			]
+			route: '/app/ita/deputies/2022/blank'
 		},
 		{
 			label: 'Senate',
-			routes: [
-				{
-					label: 'Senate 2022',
-					route: '/app/ita/senate/2022/blank'
-				},
-
-				{
-					label: '2022 Senate Results',
-					route: '/app/ita/senate/2022/results'
-				}
-			]
+			route: '/app/ita/senate/2022/blank'
 		},
 		{
 			label: 'Regions',
-			routes: [
-				{
-					label: 'Regions',
-					route: '/app/ita/regions/2023/blank'
-				},
-				{
-					label: 'Provinces',
-					route: '/app/ita/provinces/2023/blank'
-				}
-			]
+			route: '/app/ita/regions/2023/blank'
+		},
+		{
+			label: 'Chamber of Deputies 2022',
+			route: '/app/ita/deputies/2022/blank'
+		},
+		{
+			label: 'Senate 2022',
+			route: '/app/ita/senate/2022/blank'
+		},
+		{
+			label: 'Regions',
+			route: '/app/ita/regions/2023/blank'
+		},
+		{
+			label: 'Provinces',
+			route: '/app/ita/provinces/2023/blank'
+		},
+		{
+			label: '2022 Chamber of Deputies Results',
+			route: '/app/ita/deputies/2022/results'
+		},
+		{
+			label: '2022 Senate Results',
+			route: '/app/ita/senate/2022/results'
 		}
 	];
 </script>
@@ -50,6 +45,5 @@
 	bg="ita"
 	attribution="Photo by Enzo Abramo"
 	attributionLink="https://www.publicdomainpictures.net/en/view-image.php?image=118762&picture=palazzo-montecitorio-in-rome"
-	{groups}
-	square
+	{links}
 />
