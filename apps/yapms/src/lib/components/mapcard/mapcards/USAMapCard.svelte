86a89087d3160b89b6017f46b8cb63af30da3d66
Ryan <42476312+Flizzyclone@users.noreply.github.com>
2026-05-09 00:59:44 -0400

    Remove "VA Not Redistricted" variants and revert new VA map (#2226)

--- apps/yapms/src/lib/components/mapcard/mapcards/USAMapCard.svelte
+++ apps/yapms/src/lib/components/mapcard/mapcards/USAMapCard.svelte
@@ -47,16 +47,6 @@
 					label: 'MO Not Redistricted',
 					modalLabel: 'House 2026 (MO Not Redistricted)',
 					route: '/app/usa/house/2026128/no_mo_hb1'
-				},
-				{
-					label: 'VA Not Redistricted',
-					modalLabel: 'House 2026 (VA Not Redistricted)',
-					route: '/app/usa/house/2026128/no_va_hj4'
-				},
-				{
-					label: 'MO & VA Not Redistricted',
-					modalLabel: 'House 2026 (MO Not Redistricted, VA Not Redistricted)',
-					route: '/app/usa/house/2026128/no_mo_hb1_va_hj4'
 				}
 			]
 		},
