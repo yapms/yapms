a6f7803cb4788919d42840c9c1aa26cdd93e8b74
Flizzyclone <stormry20@gmail.com>
2026-05-18 17:34:56 -0400

    format

--- apps/yapms/src/lib/components/modals/candidatemodal/EditCandidateModal.svelte
+++ apps/yapms/src/lib/components/modals/candidatemodal/EditCandidateModal.svelte
@@ -72,10 +72,8 @@
 		event: Event & { currentTarget: EventTarget & HTMLInputElement },
 		index: number
 	) {
-		$CandidatesStore[candidateIndex].margins = $CandidatesStore[candidateIndex].margins.map((margin, i) =>
-			i === index
-				? { ...margin, color: event.currentTarget.value }
-				: margin
+		$CandidatesStore[candidateIndex].margins = $CandidatesStore[candidateIndex].margins.map(
+			(margin, i) => (i === index ? { ...margin, color: event.currentTarget.value } : margin)
 		);
 		$RegionsStore = $RegionsStore;
 		colorToDelete = undefined;
